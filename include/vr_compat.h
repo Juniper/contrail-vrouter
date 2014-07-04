5bb3f4ab8f579e0253e968079f039e7932c93c10
Takashi Sogabe <sogabe@iij.ad.jp>
2014-07-04 03:36:42 +0000

    Fix compile error because of changing a name of the skb related function
    From kernel version 3.14.0 and above, skb_get_rxhash() is renamed
    to skb_get_hash(). This patch fixes the compile error.

--- include/vr_compat.h
+++ include/vr_compat.h
@@ -10,6 +10,13 @@
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(3,3,0))
 typedef u64 netdev_features_t;
 #endif
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0))
+#define skb_get_rxhash skb_get_hash
+#elif ((LINUX_VERSION_CODE < KERNEL_VERSION(3,14,0)) && \
+       (LINUX_VERSION_CODE >= KERNEL_VERSION(3,13,0)) && \
+       defined(OS_LSB_Ubuntu))
+#define skb_get_rxhash skb_get_hash
+#endif
 #if (LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,32))
 static inline __u32
 skb_get_rxhash(struct sk_buff *skb)
