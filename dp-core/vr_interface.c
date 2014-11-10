247a0d66b766520e930c1b0965a562dce9d788c6
Anand H Krishnan <anandhk@juniper.net>
2014-11-09 21:53:35 -0800

    linux tunnel interface support in vrouter
    For tunnel interface support, packets that ingresses and egresses the
    tunnel interface will have network header rather than ethernet header
    as vrouter today expects. vrouter will look the encap of the device
    that is added to vrouter to see whether the interface supports an
    ethernet encap or not. For now, if the interface does not support an
    ethernet encap, vrouter will assume that the interface is a tunnel
    interface.
    
    TODO: segmentation and addition of ethernet header for packets that
    go to vhost interface during vrouter reset
    
    Bug:#1391051
    Change-Id: I47717f4a9e332b8fcfbb2878c4659a5eead54ffb

--- dp-core/vr_interface.c
+++ dp-core/vr_interface.c
@@ -86,6 +86,9 @@ vif_cmn_rewrite(struct vr_interface *vif, struct vr_packet *pkt,
 {
     unsigned char *head;
 
+    if (!len)
+        return pkt_data(pkt);
+
     if (pkt_head_space(pkt) < len) {
         pkt = vr_pexpand_head(pkt, len - pkt_head_space(pkt));
         if (!pkt)
@@ -509,8 +512,9 @@ vhost_tx(struct vr_interface *vif, struct vr_packet *pkt)
     struct vr_interface_stats *stats = vif_get_stats(vif, pkt->vp_cpu);
     unsigned short eth_proto, pull_len = 0;
     unsigned char *new_eth, *eth;
+    struct vr_eth *eth_hdr;
     struct vr_vlan_hdr *vlan;
-
+    struct vr_interface *in_vif;
 
     stats->vis_obytes += pkt_len(pkt);
     stats->vis_opackets++;
@@ -518,25 +522,36 @@ vhost_tx(struct vr_interface *vif, struct vr_packet *pkt)
     if (vif->vif_type == VIF_TYPE_XEN_LL_HOST)
         memcpy(pkt_data(pkt), vif->vif_mac, sizeof(vif->vif_mac));
     else if (vif->vif_type == VIF_TYPE_HOST) {
-
-        /* Untag any tagged packets */
-        eth = pkt_data(pkt);
-        eth_proto = ntohs(*(unsigned short *)(eth + VR_ETHER_PROTO_OFF));
-        while (eth_proto == VR_ETH_PROTO_VLAN) {
-            vlan = (struct vr_vlan_hdr *)(pkt_data(pkt) + pull_len +
-                                              VR_ETHER_HLEN);
-            eth_proto = ntohs(vlan->vlan_proto);
-            pull_len += sizeof(*vlan);
-        }
-
-        /* If there are any vlan tags */
-        if (pull_len) {
-            new_eth = pkt_pull(pkt, pull_len);
-            if (!new_eth) {
-                vr_pfree(pkt, VP_DROP_PULL);
-                return 0;
+        in_vif = pkt->vp_if;
+        if (!hif_ops->hif_get_encap ||
+                (hif_ops->hif_get_encap(in_vif) == VIF_ENCAP_TYPE_ETHER)) {
+            /* Untag any tagged packets */
+            eth = pkt_data(pkt);
+            eth_proto = ntohs(*(unsigned short *)(eth + VR_ETHER_PROTO_OFF));
+            while (eth_proto == VR_ETH_PROTO_VLAN) {
+                vlan = (struct vr_vlan_hdr *)(pkt_data(pkt) + pull_len +
+                        VR_ETHER_HLEN);
+                eth_proto = ntohs(vlan->vlan_proto);
+                pull_len += sizeof(*vlan);
             }
-            memmove(new_eth, eth, (2 * VR_ETHER_ALEN));
+
+            /* If there are any vlan tags */
+            if (pull_len) {
+                new_eth = pkt_pull(pkt, pull_len);
+                if (!new_eth) {
+                    vr_pfree(pkt, VP_DROP_PULL);
+                    return 0;
+                }
+                memmove(new_eth, eth, (2 * VR_ETHER_ALEN));
+            }
+        } else {
+            eth_hdr = (struct vr_eth *)pkt_push(pkt, VR_ETHER_HLEN);
+            memcpy(eth_hdr->eth_dmac, vif->vif_mac, VR_ETHER_ALEN);
+            memcpy(eth_hdr->eth_smac, vif->vif_mac, VR_ETHER_ALEN);
+            if (pkt->vp_type == VP_TYPE_IP6)
+                eth_hdr->eth_proto = htons(VR_ETH_PROTO_IP6);
+            else
+                eth_hdr->eth_proto = htons(VR_ETH_PROTO_IP);
         }
     }
 
@@ -761,6 +776,47 @@ vm_rx(struct vr_interface *vif, struct vr_packet *pkt,
     return vr_virtual_input(vif->vif_vrf, vif, pkt, vlan_id);
 }
 
+static int
+tun_rx(struct vr_interface *vif, struct vr_packet *pkt,
+        unsigned short vlan_id)
+{
+    unsigned char first_b;
+    struct vr_forwarding_md fmd;
+    struct vr_interface_stats *stats = vif_get_stats(vif, pkt->vp_cpu);
+
+    stats->vis_ibytes += pkt_len(pkt);
+    stats->vis_ipackets++;
+
+    if (vif_mode_xconnect(vif))
+        pkt->vp_flags |= VP_FLAG_TO_ME;
+
+    first_b = *pkt_data(pkt);
+    first_b &= 0xf0;
+
+    switch (first_b) {
+    case 0x40:
+        pkt->vp_type = VP_TYPE_IP;
+        break;
+
+    case 0x60:
+        pkt->vp_type = VP_TYPE_IP6;
+        break;
+
+    default:
+        vr_pfree(pkt, VP_DROP_INVALID_PACKET);
+        return 0;
+    }
+
+    pkt_set_network_header(pkt, pkt->vp_data);
+
+    vr_init_forwarding_md(&fmd);
+    fmd.fmd_vlan = vlan_id;
+
+    vr_l3_input(vif->vif_vrf, pkt, &fmd);
+
+    return 0;
+}
+
 
 static int
 eth_rx(struct vr_interface *vif, struct vr_packet *pkt,
@@ -940,6 +996,11 @@ eth_drv_add(struct vr_interface *vif,
     ret = hif_ops->hif_add(vif);
     if (ret)
         goto exit_add;
+
+    if ((vif->vif_type == VIF_TYPE_PHYSICAL) &&
+            (hif_ops->hif_get_encap(vif) == VIF_ENCAP_TYPE_L3)) {
+            vif->vif_rx = tun_rx;
+    }
 
     /*
      * as soon as we add the tap, packets will start traversing vrouter.
