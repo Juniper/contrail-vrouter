9d4a3d74252e0e8e49b1ecfd71401275d0ecf817
Pedro Marques <roque@juniper.net>
2015-03-31 23:58:50 +0000

    API change for alloc_netdev_mqs on 3.17
    Change-Id: I28479ca3d159636cddd640a6a815a8ac32cfadd6

--- linux/vr_host_interface.c
+++ linux/vr_host_interface.c
@@ -1820,8 +1820,15 @@ linux_pkt_dev_init(char *name, void (*setup)(struct net_device *),
     int err = 0;
     struct net_device *pdev = NULL;
 
-    if (!(pdev = alloc_netdev_mqs(0, name, setup,
-                                  1, num_present_cpus()))) {
+    pdev = alloc_netdev_mqs(0, name,
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,17,0))
+#else
+                            NET_NAME_UNKNOWN,
+#endif
+                            setup, 1, num_present_cpus());
+
+
+    if (pdev == NULL) {
         vr_module_error(-ENOMEM, __FUNCTION__, __LINE__, 0);
         return NULL;
     }
