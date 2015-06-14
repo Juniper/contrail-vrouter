6387b7145acf9659b2dfb3e964421a17eb2f3b47
Divakar <ddivakar@juniper.net>
2015-06-14 12:00:00 +0530

    Trapping router solicitation in TSN Currenty router solicitation packets are not trapped in TSN and are replicated to braodcast domains. For V6 functionality router solicitation needs to be trapped. Along with this fix, router solicitation packet is pulled in inner headers. closes-bug: #1464966
    Change-Id: I9b789f888da8e12af7cee6487e8de772f3eed0c6

--- linux/vrouter_mod.c
+++ linux/vrouter_mod.c
@@ -1244,6 +1244,9 @@ lh_pull_inner_headers_fast_udp(struct vr_packet *pkt, int
                     /* ICMP options size for neighbor solicit is 24 bytes */
                     pull_len += 24;
                 }
+                if (icmph->icmp_type == VR_ICMP6_TYPE_ROUTER_SOL) {
+                    pull_len += 8;
+                }
                 if (frag_size < pull_len)
                     goto slow_path;
 
@@ -1682,6 +1685,11 @@ lh_pull_inner_headers_fast_gre(struct vr_packet *pkt, int
                     /* ICMP options size for neighbor solicit is 24 bytes */
                     pull_len += 24;
                 }
+
+                if (icmph->icmp_type == VR_ICMP6_TYPE_ROUTER_SOL) {
+                    pull_len += 8;
+                }
+
                 if (frag_size < pull_len)
                     goto slow_path;
 
@@ -1895,9 +1903,8 @@ lh_pull_inner_headers(struct vr_packet *pkt,
      * is what pskb_may_pull() expects.
      */
     pull_len += (pkt->vp_data - (skb->data - skb->head));
-    if (!pskb_may_pull(skb, pull_len)) {
+    if (!pskb_may_pull(skb, pull_len))
         goto error;
-    }
 
     vrouter_overlay_len = VROUTER_L2_OVERLAY_LEN;
     outer_cksum_validate = false;
@@ -2006,6 +2013,7 @@ lh_pull_inner_headers(struct vr_packet *pkt,
             pull_len += sizeof(struct ipv6hdr);
             if (!pskb_may_pull(skb, pull_len))
                 goto error;
+
             ip6h = (struct vr_ip6 *) (skb->head + hoff);
             iph = NULL;
         } else if (ntohs(eth_proto) == VR_ETH_PROTO_ARP) {
@@ -2094,9 +2102,8 @@ lh_pull_inner_headers(struct vr_packet *pkt,
                     if (skb->len < pull_len)
                         pull_len = skb->len;
 
-                    if (!pskb_may_pull(skb, pull_len)) {
+                    if (!pskb_may_pull(skb, pull_len))
                         goto error;
-                    }
 
                     pull_len -= sizeof(struct vr_icmp);
                     if (icmp_pl_ip_proto == VR_IP_PROTO_TCP)
@@ -2138,9 +2145,14 @@ lh_pull_inner_headers(struct vr_packet *pkt,
                     /* ICMP options size for neighbor solicit is 24 bytes */
                     pull_len += 24;
                 }
-                if (!pskb_may_pull(skb, pull_len)) {
-                    goto error;
+
+                if (icmph->icmp_type == VR_ICMP6_TYPE_ROUTER_SOL) {
+                    pull_len += 8;
                 }
+
+                if (!pskb_may_pull(skb, pull_len))
+                    goto error;
+
                 ip6h = (struct vr_ip6 *) (skb->head + hoff);
                 iph = (struct vr_ip*) ip6h;
             }
