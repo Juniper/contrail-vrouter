d608474fc0256041e7815048cda1fd9834c0a233
Anand H. Krishnan <anandhk@juniper.net>
2015-07-02 12:29:18 +0530

    Handle out of order fragments
    vRouter, as of now, drops fragments if the head of a fragmented packet
    has not arrived yet, since it doesn't know the layer 4 port information.
    To handle such scenarios, vRouter now caches such packets till head
    of the fragmented packet arrives.
    
    Datapath enqueues each fragment to a per-cpu queue of fragments. The
    'assembler' dequeues the enqueued packets from the per-cpu queue and
    enqueues such fragments in a packet cache. We have two separate
    entities here because we wouldn't like to hold a lock in the datapath.
    Once the head of the fragment arrives, the assembler flushes all the
    held packets. There is also an aging timer that periodically scans for
    inactive entries and frees them. This is more for packet loss cases
    where some fragments don't make to the vrouter.
    
    Change-Id: I7cac445f0674c05496ad7cf1c0eff5e2fed3b6ed
    Closes-BUG: #1440230

--- include/vr_fragment.h
+++ include/vr_fragment.h
@@ -7,6 +7,12 @@
 #ifndef __VR_FRAGMENT_H__
 #define __VR_FRAGMENT_H__
 
+#define VR_ASSEMBLER_TIMEOUT_TIME               5
+#define VR_LINUX_ASSEMBLER_BUCKETS              1024
+#define VR_MAX_FRAGMENTS_PER_ASSEMBLER_QUEUE    256
+#define VR_MAX_FRAGMENTS_PER_CPU_QUEUE          256
+#define VR_FRAG_ENQUEUE_ATTEMPTS                3
+
 struct vr_fragment_key {
     unsigned int fk_sip;
     unsigned int fk_dip;
@@ -14,11 +20,27 @@ struct vr_fragment_key {
     unsigned short fk_vrf;
 } __attribute__((packed));
 
+struct vr_fragment_queue_element {
+    struct vrouter *fqe_router;
+    struct vr_fragment_queue_element *fqe_next;
+    struct vr_packet_node fqe_pnode;
+};
+
+struct vr_fragment_queue {
+    uint64_t vfq_length;
+    struct vr_fragment_queue_element *vfq_tail;
+};
+
 struct vr_fragment {
     struct vr_fragment_key f_key;
     unsigned short f_sport;
     unsigned short f_dport;
+    struct vr_fragment *f_next;
+    struct vr_fragment_queue_element *f_qe;
     uint64_t f_time;
+    uint16_t f_expected;
+    uint16_t f_received;
+    bool f_port_info_valid;
 } __attribute__((packed));
 
 #define f_sip f_key.fk_sip
@@ -33,5 +55,13 @@ struct vr_fragment *vr_fragment_get(struct vrouter *, unsigned short,
 int vr_fragment_add(struct vrouter *, unsigned short, struct vr_ip *,
                 unsigned short, unsigned short);
 void vr_fragment_del(struct vr_fragment *);
+uint32_t vr_fragment_get_hash(unsigned int, struct vr_packet *);
+int vr_fragment_assembler(struct vr_fragment **,
+        struct vr_fragment_queue_element *);
+unsigned int vr_assembler_table_scan(struct vr_fragment **);
+int vr_fragment_enqueue(struct vrouter *, struct vr_fragment_queue *,
+        struct vr_packet *, struct vr_forwarding_md *);
+int vr_assembler_table_scan_init(void (*)(void *));
+void vr_assembler_table_scan_exit(void);
 
 #endif /* __VR_FRAGMENT_H__ */
