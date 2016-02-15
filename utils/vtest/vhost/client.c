61d28af068c04ca5d1c9677e10457dc42cd73d99
rch <rch@semihalf.com>
2016-02-15 16:38:23 +0100

    Vtest, vhost_net, prepared for reviewing process.
    Change-Id: I7ba6bef54b0af6531553aa92882693b9888cfd02
    Closes-bug: #1544091

---
+++ utils/vtest/vhost/client.c
@@ -0,0 +1,472 @@
+/*
+ * client.c
+ *
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+*/
+
+#include <errno.h>
+#include <libgen.h>
+#include <linux/vhost.h>
+#include <limits.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <linux/un.h>
+
+
+#include "client.h"
+#include "util.h"
+#include "virtio_hdr.h"
+
+int
+client_init_Client(Client *client, const char *path) {
+
+    CLIENT_H_RET_VAL client_ret_val = E_CLIENT_OK;
+    UTILS_H_RET_VAL utils_ret_val = E_UTILS_OK;
+
+    struct timeval tm;
+
+    if (!client || !path) {
+        return E_CLIENT_ERR_FARG;
+    }
+
+    tm.tv_sec = 0;
+    tm.tv_usec = 500;
+
+    client_ret_val = client_init_path(client, path);
+    if (client_ret_val != E_CLIENT_OK) {
+        return client_ret_val;
+    }
+
+    client_ret_val = client_init_socket(client);
+    if (client_ret_val != E_CLIENT_OK) {
+        return client_ret_val;
+    }
+
+    client_ret_val = client_connect_socket(client);
+    if (client_ret_val != E_CLIENT_OK) {
+        return client_ret_val;
+    }
+/*
+ * Actually we do need this
+    utils_ret_val = utils_init_fd_rw_t(&client->fd_rw_list, tm);
+    if (utils_ret_val != E_UTILS_OK) {
+        return E_CLIENT_ERR;
+    }
+*/
+    memset(&client->sh_mem_fds,
+           -2, sizeof(int) * VHOST_MEMORY_MAX_NREGIONS);
+
+    return E_CLIENT_OK;
+}
+
+
+int
+client_init_path(Client *client, const char *path) {
+
+    char *basename_path = NULL;
+
+    if (!client || !path || strlen(path) == 0 || strlen(path) > (UNIX_PATH_MAX - 1)) {
+        return E_CLIENT_ERR_FARG;
+    }
+   basename_path = basename((char *)path);
+
+   strncpy(client->socket_path, path, strlen(path));
+
+   strncpy(client->sh_mem_path, basename_path, strlen(basename_path));
+
+
+   return E_CLIENT_OK;
+}
+
+int
+client_init_socket(Client *client) {
+
+    if (!client) {
+        return E_CLIENT_ERR_FARG;
+    }
+
+    client->socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (client->socket == -1) {
+
+        return E_CLIENT_ERR_SOCK;
+    }
+    return E_CLIENT_OK;
+
+}
+
+int
+client_connect_socket(Client *client) {
+
+    struct sockaddr_un unix_socket;
+    size_t addrlen = 0;
+    struct stat unix_socket_stat;
+    if (!client->socket || strlen(client->socket_path) == 0) {
+        return E_CLIENT_ERR_FARG;
+    }
+
+    if (!(stat(client->socket_path, &unix_socket_stat) == 0
+                && S_ISSOCK(unix_socket_stat.st_mode))) {
+
+        return E_CLIENT_ERR_CONN;
+    }
+
+    memset(&unix_socket, 0, sizeof(struct sockaddr_un));
+
+    unix_socket.sun_family = AF_UNIX;
+    strncpy(unix_socket.sun_path, client->socket_path, sizeof(unix_socket.sun_path) - 1);
+    addrlen = strlen(unix_socket.sun_path) + sizeof(AF_UNIX);
+
+    if (connect(client->socket, (struct sockaddr *)&unix_socket, addrlen)  == -1) {
+
+        return E_CLIENT_ERR_CONN;
+    }
+
+    return E_CLIENT_OK;
+}
+
+int
+client_disconnect_socket(Client *client) {
+
+    if (!client) {
+        return E_CLIENT_ERR_FARG;
+    }
+
+    if (!(client->socket < 0)) {
+        close(client->socket);
+        client->socket = -2;
+    }
+    return E_CLIENT_OK;
+}
+
+int
+client_close_fds(Client *client) {
+
+    if (!client) {
+        return E_CLIENT_ERR_FARG;
+    }
+
+    for (size_t i = 0; i < VHOST_MEMORY_MAX_NREGIONS; i++) {
+        if (client->sh_mem_fds[i] >= 0 ) {
+            close(client->sh_mem_fds[i]);
+            client->sh_mem_fds[i] = -2;
+        }
+    }
+    return E_CLIENT_OK;
+}
+
+int
+client_vhost_ioctl(Client *client, VhostUserRequest request, void *req_ptr) {
+
+    Client *const cl = client;
+    int fds[VHOST_MEMORY_MAX_NREGIONS] = {-2};
+    VhostUserMsg message;
+    CLIENT_H_RET_VAL ret_val = E_CLIENT_OK;
+    CLIENT_H_RET_VAL ret_set_val = E_CLIENT_VIOCTL_REPLY;
+    size_t fd_num = 0;
+
+    if (!client) {
+        return E_CLIENT_ERR_FARG;
+    }
+
+    /* Function argument pointer (req_ptr) for following messages
+     * SHOULD not be NULL. */
+    switch (request) {
+        case VHOST_USER_SET_MEM_TABLE:
+        case VHOST_USER_SET_LOG_BASE:
+        case VHOST_USER_SET_LOG_FD:
+        case VHOST_USER_SET_VRING_KICK:
+        case VHOST_USER_SET_VRING_CALL:
+        case VHOST_USER_SET_VRING_ERR:
+            if (!req_ptr) {
+                return E_CLIENT_ERR_FARG;
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    memset(&message, 0, sizeof(VhostUserMsg));
+
+    message.request = request;
+    message.flags &= ~VHOST_USER_VERSION_MASK;
+    message.flags |= QEMU_PROT_VERSION;
+
+    /* Set message structure for sending data. */
+    ret_set_val = client_vhost_ioctl_set_send_msg(cl, request, req_ptr, &message, fds, &fd_num);
+
+    if (!(ret_set_val == E_CLIENT_OK || ret_set_val == E_CLIENT_VIOCTL_REPLY)) {
+        return E_CLIENT_ERR_VIOCTL;
+    }
+
+    ret_val = client_vhost_ioctl_send_fds(&message, cl->socket, fds, fd_num );
+    if (ret_val != E_CLIENT_OK) {
+        return ret_val;
+    }
+
+    if (ret_set_val == E_CLIENT_VIOCTL_REPLY) {
+
+        /* Set message structure after receive data */
+        ret_val = client_vhost_ioctl_set_recv_msg(request, req_ptr, &message);
+        if (!(ret_val == E_CLIENT_OK)) {
+            return E_CLIENT_ERR_VIOCTL;
+        }
+
+        ret_val = client_vhost_ioctl_recv_fds(cl->socket, &message, fds, &fd_num);
+
+        if (ret_val != E_CLIENT_OK) {
+            return ret_val;
+        }
+    }
+
+    return E_CLIENT_OK;
+}
+
+int
+client_vhost_ioctl_set_send_msg(Client *client, VhostUserRequest request, void *req_ptr,
+                     VhostUserMsg *msg, int *fds, size_t *fd_num ) {
+
+    VhostUserMsg *const message = msg;
+    bool msg_has_reply = false;
+
+    size_t *const l_fd_num = fd_num;
+    struct vring_file {unsigned int index; int fd;} *file;
+
+    if (!client || !msg || !fds || !fd_num) {
+
+        return E_CLIENT_ERR_FARG;
+    }
+
+
+    switch (request) {
+
+        case VHOST_USER_NONE:
+            break;
+
+        case VHOST_USER_GET_FEATURES:
+        case VHOST_USER_GET_VRING_BASE:
+            msg_has_reply = true;
+            break;
+
+        case VHOST_USER_SET_FEATURES:
+        case VHOST_USER_SET_LOG_BASE:
+            message->u64 = *((uint64_t *) req_ptr);
+            message->size = sizeof(((VhostUserMsg*)0)->u64);
+            /* if VHOST_USER_PROTOCOL_F_LOG_SHMFD
+            msg_has_reply = true;
+            */
+            break;
+
+        case VHOST_USER_SET_OWNER:
+        case VHOST_USER_RESET_OWNER:
+            break;
+
+        case VHOST_USER_SET_MEM_TABLE:
+            memcpy(&message->memory, req_ptr, sizeof(VhostUserMemory));
+            message->size = sizeof(((VhostUserMemory*)0)->padding);
+            message->size += sizeof(((VhostUserMemory*)0)->nregions);
+
+            for (*l_fd_num = 0; *l_fd_num < message->memory.nregions; (*l_fd_num)++) {
+                fds[*l_fd_num] = client->sh_mem_fds[*l_fd_num];
+                message->size = message->size + sizeof(VhostUserMemoryRegion);
+            }
+            break;
+
+        case VHOST_USER_SET_LOG_FD:
+            fds[++(*l_fd_num)] = *((int *) req_ptr);
+            break;
+
+        case VHOST_USER_SET_VRING_NUM:
+        case VHOST_USER_SET_VRING_BASE:
+            memcpy(&message->state, req_ptr, sizeof(((VhostUserMsg*)0)->state));
+            message->size = sizeof(((VhostUserMsg*)0)->state);
+            break;
+
+        case VHOST_USER_SET_VRING_ADDR:
+            memcpy(&message->addr, req_ptr, sizeof(((VhostUserMsg*)0)->addr));
+            message->size = sizeof(((VhostUserMsg*)0)->addr);
+            break;
+
+        case VHOST_USER_SET_VRING_KICK:
+        case VHOST_USER_SET_VRING_CALL:
+        case VHOST_USER_SET_VRING_ERR:
+            file = req_ptr;
+            message->u64 = file->index;
+            message->size = sizeof(((VhostUserMsg*)0)->u64);
+            if (file->fd > 0 ) {
+                fds[(*l_fd_num)++] = file->fd;
+            }
+            break;
+
+        default:
+            return E_CLIENT_ERR_IOCTL_SEND;
+
+    }
+    if (msg_has_reply) {
+       return E_CLIENT_VIOCTL_REPLY;
+    }
+
+    return E_CLIENT_OK;
+}
+
+int
+client_vhost_ioctl_set_recv_msg(VhostUserRequest request, void *req_ptr, VhostUserMsg *msg) {
+
+    VhostUserMsg *const message = msg;
+
+    if (!msg) {
+
+        return E_CLIENT_ERR_FARG;
+    }
+
+    switch (request) {
+        case VHOST_USER_GET_FEATURES:
+            *((uint64_t *) req_ptr) = message->u64;
+            break;
+        case VHOST_USER_GET_VRING_BASE:
+            memcpy(req_ptr, &message->state, sizeof(struct vhost_vring_state));
+
+        default:
+            return E_CLIENT_ERR_IOCTL_REPLY;
+    }
+
+    return E_CLIENT_OK;
+}
+
+int
+client_vhost_ioctl_send_fds(VhostUserMsg *msg, int fd, int *fds, size_t fd_num) {
+
+    struct iovec iov;
+    struct msghdr msgh;
+    struct cmsghdr *cmsgh = NULL;
+    char controlbuf[CMSG_SPACE(fd_num * sizeof(int))];
+
+    size_t vhost_user_msg_member_size = ((sizeof(((VhostUserMsg*)0)->request)) +
+    (sizeof(((VhostUserMsg*)0)->flags)) + (sizeof(((VhostUserMsg*)0)->size)));
+
+    const VhostUserMsg *const message = msg;
+    int ret = 0;
+
+    if (!msg || !fds) {
+        return E_CLIENT_ERR_FARG;
+    }
+
+    memset(controlbuf, 0, sizeof(controlbuf));
+    memset(&msgh, 0, sizeof(struct msghdr));
+    memset(&iov, 0, sizeof(struct iovec));
+
+    iov.iov_base = (void *) message;
+    iov.iov_len = vhost_user_msg_member_size + message->size;
+
+    msgh.msg_iov = &iov;
+    msgh.msg_iovlen = 1;
+
+    if (fd_num) {
+        msgh.msg_name = NULL;
+        msgh.msg_namelen = 0;
+        msgh.msg_control = controlbuf;
+        msgh.msg_controllen = sizeof(controlbuf);
+
+        cmsgh = CMSG_FIRSTHDR(&msgh);
+        cmsgh->cmsg_len = CMSG_LEN(sizeof(int) * fd_num);
+        cmsgh->cmsg_level = SOL_SOCKET;
+        cmsgh->cmsg_type = SCM_RIGHTS;
+
+
+        memcpy(CMSG_DATA(cmsgh), fds, sizeof(int) * fd_num);
+
+    } else {
+        msgh.msg_control = NULL;
+        msgh.msg_controllen = 0;
+    }
+
+    do {
+        ret = sendmsg(fd, &msgh, 0);
+
+    } while (ret < 0 && errno == EINTR);
+
+    if (ret < 0) {
+        return E_CLIENT_ERR_IOCTL_SEND;
+    }
+
+    return E_CLIENT_OK;
+}
+
+int
+client_vhost_ioctl_recv_fds(int fd, VhostUserMsg *msg, int *fds, size_t *fd_num) {
+
+    struct  msghdr msgh;
+    struct iovec iov;
+    struct cmsghdr *cmsgh = NULL;
+    char controlbuf[CMSG_SPACE(sizeof(int) * (*fd_num))];
+    VhostUserMsg *const message = msg;
+    int *const l_fds = fds;
+    size_t *const l_fd_num = fd_num;
+    int ret = 0;
+
+    if (!msg || !fds || !fd_num) {
+
+        return E_CLIENT_ERR_FARG;
+    }
+
+    memset(controlbuf, 0, sizeof(controlbuf));
+    memset(&msgh, 0, sizeof(struct msghdr));
+    memset(&iov, 0, sizeof(struct iovec));
+
+    msgh.msg_name = NULL;
+    msgh.msg_namelen = 0;
+
+    msgh.msg_iov = &iov;
+    msgh.msg_iovlen = 1;
+    msgh.msg_control = controlbuf;
+    msgh.msg_controllen = sizeof(controlbuf);
+
+    iov.iov_base = (void *) message;
+    iov.iov_len = VHOST_USER_HDR_SIZE;
+
+    ret = recvmsg(fd, &msgh, 0);
+
+    if ((ret > 0 && msgh.msg_flags & (MSG_TRUNC | MSG_CTRUNC)) || ret < 0 ) {
+       return E_CLIENT_ERR_IOCTL_REPLY;
+    }
+
+    cmsgh = CMSG_FIRSTHDR(&msgh);
+    if (cmsgh && cmsgh->cmsg_len > 0 && cmsgh->cmsg_level == SOL_SOCKET &&
+            cmsgh->cmsg_type == SCM_RIGHTS) {
+
+        client_vhost_ioctl_recv_fds_handler(cmsgh, l_fds, l_fd_num);
+
+    }
+
+    read(fd, ((char*)msg) + ret, message->size);
+    return E_CLIENT_OK;
+}
+
+int
+client_vhost_ioctl_recv_fds_handler(struct cmsghdr *cmsgh, int *fds, size_t *fd_num) {
+
+    struct cmsghdr *const l_cmsgh = cmsgh;
+    int *const l_fds = fds;
+    size_t *const l_fd_num = fd_num;
+    size_t fd_size = 0;
+
+    if (!cmsgh || !fds || !fd_num) {
+        return E_CLIENT_ERR_FARG;
+    }
+
+    if (*fd_num * sizeof(int) >= l_cmsgh->cmsg_len - CMSG_LEN(0)) {
+       fd_size = l_cmsgh->cmsg_len - CMSG_LEN(0);
+       *l_fd_num = fd_size / sizeof(int);
+       memcpy(l_fds, CMSG_DATA(l_cmsgh), fd_size);
+    }
+
+    return E_CLIENT_OK;
+}
+
+
+
---
+++ utils/vtest/vhost/virt_queue.h
@@ -0,0 +1,154 @@
+/*
+ * virt_queue.h - some headers, functions and message definitions are copied from
+ * reference file (virtio_ring.h)
+ *
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+*/
+#ifndef VIRT_QUEUE_H
+#define VIRT_QUEUE_H
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "uvhost.h"
+#include "virtio_hdr.h"
+
+#define VIRTQ_IDX_NONE ((uint16_t)-1)
+
+/* Maximum is set to value 2^15 due to max value idx in ring */
+#define VIRTQ_DESC_MAX_SIZE (32768)
+
+/* Maximal ethernet MTU + sizeof(virtio_net_hdr) */
+#define VIRTQ_DESC_BUFF_SIZE (ETH_MAX_MTU + sizeof(struct virtio_net_hdr))
+#define ETH_MAX_MTU (1514)
+
+
+typedef enum virtq_desc_flags {
+
+    /* This marks a buffer as continuing via the next field. */
+    VIRTIO_DESC_F_NEXT = 1,
+
+    /* This marks a buffer as write-only (otherwise read-only). */
+    VIRTIO_DESC_F_WRITE = 2,
+
+    /* This means the buffer contains a list of buffer descriptors. */
+    VIRTIO_DESC_F_INDIRECT = 4
+
+} virtq_desc_flags;
+
+
+/* Virtqueue descriptors: 16 bytes.
+ *  * These can chain together via "next". */
+struct virtq_desc {
+    /* Address (guest-physical). */
+    uint64_t addr;
+    /* Length. */
+    uint32_t len;
+    /* The flags as indicated above. */
+    uint16_t flags;
+    /* We chain unused descriptors via this, too */
+    uint16_t next;
+};
+
+struct virtq_avail {
+    uint16_t flags;
+    uint16_t idx;
+    uint16_t ring[VIRTQ_DESC_MAX_SIZE];
+    /* Only if VIRTIO_F_EVENT_IDX: le16 used_event; */
+};
+
+/* le32 is used here for ids for padding reasons. */
+struct virtq_used_elem {
+    /* Index of start of used descriptor chain. */
+    uint32_t id;
+    /* Total length of the descriptor chain which was written to. */
+    uint32_t len;
+};
+
+struct virtq_used {
+    uint16_t flags;
+    uint16_t idx;
+    struct virtq_used_elem ring[VIRTQ_DESC_MAX_SIZE];
+};
+
+typedef struct uvhost_virtq {
+    int kickfd;
+    int callfd;
+    struct virtq_desc desc[VIRTQ_DESC_MAX_SIZE] __attribute__((aligned(4)));
+    struct virtq_avail avail                    __attribute__((aligned(2)));
+    struct virtq_used used                      __attribute__((aligned(4096)));
+} uvhost_virtq;
+
+struct virtq {
+    unsigned int num;
+    struct virtq_desc *desc;
+    struct virtq_avail *avail;
+    struct virtq_used *used;
+};
+
+typedef struct virtq_control {
+    struct virtq virtq;
+    uint16_t last_used_idx;
+    uint16_t last_avail_idx;
+    int kickfd;
+    int callfd;
+
+} virtq_control;
+
+struct set_host_virtq {
+    struct vhost_vring_state num;
+    struct vhost_vring_state base;
+    struct vhost_vring_file kick;
+    struct vhost_vring_file call;
+    struct vhost_vring_addr addr;
+} set_virtq_init;
+
+typedef enum {
+    E_VIRT_QUEUE_OK = EXIT_SUCCESS,
+    E_VIRT_QUEUE_ERR_ALLOC,
+    E_VIRT_QUEUE_ERR_UNK,
+    E_VIRT_QUEUE_ERR_FARG,
+    E_VIRT_QUEUE_ERR_HOST_VIRTQ,
+    E_VIRT_QUEUE_ERR_MAP_REG,
+    E_VIRT_QUEUE_ERR_SEND_PACKET,
+    E_VIRT_QUEUE_ERR_RECV_PACKET,
+    E_VIRT_QUEUE_ERR_ALLOC_PACKET,
+    E_VIRT_QUEUE_LAST
+} VIRT_QUEUE_H_RET_VAL;
+
+int virt_queue_put_tx_virt_queue(struct virtq_control **virtq_control, VHOST_CLIENT_VRING vq_id, void *src_buf, size_t src_buf_len);
+int virt_queue_put_rx_virt_queue(struct virtq_control **virtq_control, VHOST_CLIENT_VRING vq_id, size_t src_buf_len);
+int virt_queue_kick(struct virtq_control **virtq_control, VHOST_CLIENT_VRING vq_id);
+int virt_queue_process_used_tx_virt_queue(struct virtq_control **virtq_control, VHOST_CLIENT_VRING vq_id);
+int virt_queue_process_used_rx_virt_queue(struct virtq_control **virtq_control, VHOST_CLIENT_VRING vq_id);
+int virt_queue_free_virt_queue(struct virtq_control **virtq_control, VHOST_CLIENT_VRING vq_id, uint32_t desc_idx);
+int virt_queue_process_desc_virt_queue(struct virtq_control **virtq_control, VHOST_CLIENT_VRING vq_id, uint32_t avail_idx);
+int virt_queue_process_avail_virt_queue(struct virtq_control **virtq_control ,VHOST_CLIENT_VRING vq_id);
+int virt_queue_map_uvhost_virtq_2_virtq_control(Vhost_Client *vhost_client);
+
+int virt_queue_map_all_mem_reqion_virtq(struct uvhost_virtq **virtq, VhostUserMemory *mem,
+                                        size_t virtq_number);
+int virt_queue_map_vring(struct uvhost_virtq **virtq, void *base_virtq);
+int virt_queue_map_mem_reqion_virtq(struct uvhost_virtq **virtq, uint64_t guest_phys_addr);
+
+//int virt_queue_set_host_vring(Client *client, struct set_host_virtq set_virtq);
+int virt_queue_set_host_virtq_table(uvhost_virtq **virtq, size_t virtq_table_size, Client *client);
+
+static inline int virtq_need_event(uint16_t event_idx, uint16_t new_idx, uint16_t old_idx)
+{
+    return (uint16_t)(new_idx - event_idx - 1) < (uint16_t)(new_idx - old_idx); 
+}
+
+/* Get location of event indices (only with VIRTIO_F_EVENT_IDX) */
+static inline uint16_t *virtq_used_event(struct virtq *vq)
+{
+    /* For backwards compat, used event index is at *end* of avail ring. */
+    return &vq->avail->ring[vq->num];
+}
+
+static inline uint16_t *virtq_avail_event(struct virtq *vq)
+{
+    /* For backwards compat, avail event index is at *end* of used ring. */
+    return (uint16_t *)&vq->used->ring[vq->num];
+}
+#endif
+
