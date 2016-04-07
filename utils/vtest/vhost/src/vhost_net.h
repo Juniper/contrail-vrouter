2eed78f3e50de82189ec28c7d404dd3f6f7fbc10
rch <rch@semihalf.com>
2016-04-07 16:39:53 +0200

    Vtest, vhost_net prepared for reviewing process.
    Closes-bug: #1544091
    Change-Id: Iad1e05d25a78bcfb6410b8f000badcb99f667cb1

---
+++ utils/vtest/vhost/src/vhost_net.h
@@ -0,0 +1,38 @@
+/*ome headers, functions and message definitions are copied from
+ * reference file (virtio_ring.h)
+    *  *
+    *   * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+    *   */
+#ifndef VHOST_NET_H
+#define VHOST_NET_H
+
+#include <stdlib.h>
+
+typedef enum {
+
+    E_VHOST_NET_OK = EXIT_SUCCESS,
+    E_VHOST_NET_ERR_FARG,
+    E_VHOST_NET_ERR_SEND_PACKET,
+    E_VHOST_NET_ERR_ALLOC,
+    E_VHOST_NET_ERR_ALLOC_PACKET,
+    E_VHOST_NET_ERR_RECV_PACKET,
+    E_VHOST_NET_ERR_INIT_COMMUNICATION,
+    E_VHOST_NET_ERR_SET_SH_MEM,
+    E_VHOST_NET_ERR_MAP_VIRTQ,
+    E_VHOST_NET_ERR_UNK,
+
+} vhost_net_state;
+
+typedef int (*tx_rx_packet_handler)(void *context, void *src_buf, size_t *src_buf_len);
+
+typedef struct vhost_net {
+    tx_rx_packet_handler rx;
+    tx_rx_packet_handler tx;
+    void *context;
+
+} vhost_net;
+
+int init_vhost_net(vhost_net **client,  const char *vhost_client_path );
+int deinit_vhost_net(vhost_net *client);
+#endif
+
