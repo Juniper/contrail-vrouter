14f0f112496145de880d0c398ff1d7b5d158f1b2
Divakar <ddivakar@juniper.net>
2016-05-27 15:15:23 +0530

    Manipulating the TTL of the Packet
    Right now the TTL of the packet is not overwritten by Vrouter. It is
    only decremented like a hop, for the required packets. But BGP packets
    in VM (due to BGP as service), can come to Vrouter to with ttl of 1. In
    a multi hop environment, we require this ttl to be more than 1. For this
    purpose flow entry is added with another ttl field and if Agent sets
    this, vrouter unconditionally sets this ttl in the packet and calculates
    the checksum again.
    
    partial-bug: #1567586
    
    Change-Id: I53a192666731f4c3662e3791006dd7294bccf116

--- utils/flow.c
+++ utils/flow.c
@@ -1405,6 +1405,7 @@ flow_dump_table(struct flow_table *ft)
                     printf(", %d, ", fe->fe_sec_mirror_id);
             }
             printf(" SPort %d", fe->fe_udp_src_port);
+            printf(" TTL %d", fe->fe_ttl);
             printf(")");
         }
 
