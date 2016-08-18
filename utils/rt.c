79b969bc6b5b4458ebb9df0a349def2b4c438496
Divakar <ddivakar@juniper.net>
2016-08-18 10:41:56 +0530

    Update prefix len in Mtrie bucket as well
    Right now, if mtrie already contains more specific entries and if a less
    specific entry is added, only non-matching leaves are updated with less
    specific entry's prefix len. The matching buckets also need to be
    updated with prefix len. Not updating the prefix len in buckets, results
    in, not collapsing the tree, if these less specific entries are deleted.
    
    For example, in an empty tree, 8.1.1.10/32 -> nh1 is added. This results
    in, at level 1 bucket 8, level 2 bucket 1, level 3 bucket 1 to contain
    prefix len as 0 and level 4 bucket 10 to contain prefix len as 32. The
    other leaves in level 4 (like 8.1.1.1 till 8.1.1.255 except 10) also
    contain prefix len as 0. After this 8.1.1.0/24 -> nh2 is added.
    Currently the prefix len of 24 is updated only in the leaves like, 8.1.1.1
    till 8.1.1.255. Ideally the prefix len 24 should be updated in level 3
    bucket 1 as well. Not updating this, leads to issues while collapsing
    level 3 buckets.
    
    Fixes:
    1) Prefix len is updated in the matching buckets as well
    2) In the label flags, if LABEL_VALID flag not set, the label stored
       does not have any meaning. But while collapsing the bucket the labels
       are also compared leading to non-collapse. The labels need to be
       compared only if the LABEL_VALID flag is set. To over come this, if
       the LABEL_VALID is not set, the label is stored as -1, so that they
       are comparable even if flag is not set
    3) In 'rt' utility, the replacment len incase of delete is wrongly
       initialised to 100. This is rectified.
    
    Change-Id: I837a32ecc8d6c15fa8644028d63ef38f2259e3ab
    closes-bug: #1605748

--- utils/rt.c
+++ utils/rt.c
@@ -60,7 +60,7 @@ static char *cmd_prefix_string, *cmd_plen_string;
 static uint8_t cmd_prefix[16], cmd_src[16];
 static uint32_t cmd_plen = 0;
 static int32_t cmd_label;
-static uint32_t cmd_replace_plen = 100;
+static uint32_t cmd_replace_plen = 0xFFFFFFFF;
 static char cmd_dst_mac[6];
 static bool dump_pending;
 
@@ -394,8 +394,11 @@ validate_options(void)
 
     case SANDESH_OP_DELETE:
         if ((cmd_family_id == AF_INET) || (cmd_family_id == AF_INET6)) {
-            if ((cmd_replace_plen < 0 ||
-                ( cmd_replace_plen > (RT_IP_ADDR_SIZE(cmd_family_id)*4)))) {
+
+            if (cmd_replace_plen == 0xFFFFFFFF)
+                goto usage_internal;
+
+            if (cmd_replace_plen > (RT_IP_ADDR_SIZE(cmd_family_id) * 8)) {
                 goto usage_internal;
             }
 
