e894f7e028afd0a879d52f4f68a7daf1c56dedf6
Divakar D <ddivakar@juniper.net>
2016-10-08 19:25:38 +0530

    Transmit V6 Neighbour advertisement on the receiving interface
    This fix is IPV6 equivalent of
    https://review.opencontrail.org/#/c/11506/.
    
    In in-network service instance case, when service scaling
    is more than one, all the instances are going to have the
    same mac address and IP address. Inet route points to
    ECMP composite nexthop but FDB route does not point to ECMP
    nexthop. So when neighbour request is received from service VM
    Vrouter needs to send response on the incoming interface
    rather bridging the reply packet, as FDB route points
    to arbitrarly any of the service VM encap L2 nexthop.
    
    After preparing the reply packet,bridge lookup is done
    and outgoing interface is compared against ingress interface.
    If they are not same, reply packet is transmitted on
    ingress interface.
    
    Change-Id: Ib0113227019d7a452541129843813a647d388b36
    closes-bug: #1461882

--- include/vr_datapath.h
+++ include/vr_datapath.h
@@ -60,6 +60,7 @@ mac_response_t vm_neighbor_request(struct vr_interface *, struct vr_packet *,
 extern int vif_plug_mac_request(struct vr_interface *, struct vr_packet *,
         struct vr_forwarding_md *);
 int vr_gro_input(struct vr_packet *, struct vr_nexthop *);
+void vr_mac_reply_send(struct vr_packet *, struct vr_forwarding_md *);
 
 
 
