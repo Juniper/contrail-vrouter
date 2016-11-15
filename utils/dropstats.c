b09e7384f19d02e19c74c9973e94db8ab8b81b91
Trevor Patrie <tpatrie@netronome.com>
2016-11-15 11:39:52 -0500

    dp_core: Introduce offloads module
    Smart nics may have ability to parse and act the Vrouter tunneled packets.
    
    So, according to the Vrouter tunnels, aka MPLSoGRE, MPLSoUDP, and VXLAN, the
    inner packet 5-tupples and the outer packet specifications, the HW can do
    some actions. For example, the HW can detect MPLSoGRE packets and to do RSS
    on the inner packet 5-tupples or just tag the packets based on the packet HW parser.
    
    Using this HW feature the Vrouter parformance can be improved.
    
    Add a Vrouter offloads module to use the described HW ability.
    
    Partial-Bug: #1781402
    Change-Id: I337dd1d25671271528be00a5fef2eebddf1b0292
    Signed-off-by: matan <matan@mellanox.com>

--- utils/dropstats.c
+++ utils/dropstats.c
@@ -27,13 +27,91 @@
 #include "ini_parser.h"
 
 static struct nl_client *cl;
-static int help_set, core_set;
+static int help_set, core_set, offload_set;
 static unsigned int core = (unsigned)-1;
 
 static void
 drop_stats_req_process(void *s_req)
 {
     vr_drop_stats_req *stats = (vr_drop_stats_req *)s_req;
+    int platform = get_platform();
+    if (core == (unsigned)-2)
+        printf("Statistics for NIC offloads\n\n");
+    else if (core != (unsigned)-1)
+        printf("Statistics for core %u\n\n", core);
+    printf("Invalid ARPs                  %" PRIu64 "\n",
+            stats->vds_invalid_arp);
+    printf("\n");
+
+    printf("Invalid IF                    %" PRIu64 "\n",
+            stats->vds_invalid_if);
+    printf("Trap No IF                    %" PRIu64 "\n",
+            stats->vds_trap_no_if);
+    printf("IF TX Discard                 %" PRIu64 "\n",
+            stats->vds_interface_tx_discard);
+    printf("IF Drop                       %" PRIu64 "\n",
+            stats->vds_interface_drop);
+    printf("IF RX Discard                 %" PRIu64 "\n",
+            stats->vds_interface_rx_discard);
+    printf("\n");
+
+    printf("Flow Unusable                 %" PRIu64 "\n",
+            stats->vds_flow_unusable);
+    printf("Flow No Memory                %" PRIu64 "\n",
+            stats->vds_flow_no_memory);
+    printf("Flow Table Full               %" PRIu64 "\n",
+            stats->vds_flow_table_full);
+    printf("Flow NAT no rflow             %" PRIu64 "\n",
+            stats->vds_flow_nat_no_rflow);
+    printf("Flow Action Drop              %" PRIu64 "\n",
+            stats->vds_flow_action_drop);
+    printf("Flow Action Invalid           %" PRIu64 "\n",
+            stats->vds_flow_action_invalid);
+    printf("Flow Invalid Protocol         %" PRIu64 "\n",
+            stats->vds_flow_invalid_protocol);
+    printf("Flow Queue Limit Exceeded     %" PRIu64 "\n",
+            stats->vds_flow_queue_limit_exceeded);
+    printf("New Flow Drops                %" PRIu64 "\n",
+            stats->vds_drop_new_flow);
+    printf("Flow Unusable (Eviction)      %" PRIu64 "\n",
+            stats->vds_flow_evict);
+    printf("\n");
+
+    printf("Discards                      %" PRIu64 "\n",
+            stats->vds_discard);
+    printf("TTL Exceeded                  %" PRIu64 "\n",
+            stats->vds_ttl_exceeded);
+    printf("Mcast Clone Fail              %" PRIu64 "\n",
+            stats->vds_mcast_clone_fail);
+    printf("Cloned Original               %" PRIu64 "\n",
+            stats->vds_cloned_original);
+    printf("\n");
+
+    printf("Invalid NH                    %" PRIu64 "\n",
+            stats->vds_invalid_nh);
+    printf("Invalid Label                 %" PRIu64 "\n",
+            stats->vds_invalid_label);
+    printf("Invalid Protocol              %" PRIu64 "\n",
+            stats->vds_invalid_protocol);
+    printf("Rewrite Fail                  %" PRIu64 "\n",
+            stats->vds_rewrite_fail);
+    printf("Invalid Mcast Source          %" PRIu64 "\n",
+            stats->vds_invalid_mcast_source);
+    printf("\n");
+
+    printf("Push Fails                    %" PRIu64 "\n",
+            stats->vds_push);
+    printf("Pull Fails                    %" PRIu64 "\n",
+            stats->vds_pull);
+    printf("Duplicated                    %" PRIu64 "\n",
+            stats->vds_duplicated);
+    printf("Head Alloc Fails              %" PRIu64 "\n",
+            stats->vds_head_alloc_fail);
+    printf("PCOW fails                    %" PRIu64 "\n",
+            stats->vds_pcow_fail);
+    printf("Invalid Packets               %" PRIu64 "\n",
+            stats->vds_invalid_packet);
+    printf("\n");
 
     vr_print_drop_stats(stats, core);
     return;
@@ -66,6 +144,11 @@ vr_get_drop_stats(struct nl_client *cl)
      * conversion between those enumerating systems. The dropstats utility
      * increments by 1 the core number user asked for. Then it is
      * decremented back in vRouter.
+     *
+     * vRouter will return only the offloaded dropstats if the "core"
+     * is passed in as -2.  This allows returning of only dropstats offloaded
+     * on NIC using this same mechanism.  If all CPUs are requested, the
+     * offloaded dropstats are included.
      */
     ret = vr_send_drop_stats_get(cl, 0, core + 1);
     if (ret < 0)
@@ -81,21 +164,28 @@ vr_get_drop_stats(struct nl_client *cl)
 enum opt_index {
     HELP_OPT_INDEX,
     CORE_OPT_INDEX,
+    OFFL_OPT_INDEX,
     MAX_OPT_INDEX,
 };
 
 static struct option long_options[] = {
     [HELP_OPT_INDEX]    =   {"help",    no_argument,        &help_set,      1},
     [CORE_OPT_INDEX]    =   {"core",    required_argument,  &core_set,      1},
-    [MAX_OPT_INDEX]     =   {NULL,    0,                  0,              0},
+    [OFFL_OPT_INDEX]    =   {"offload", no_argument,        &offload_set,   1},
+    [MAX_OPT_INDEX]     =   {"NULL",    0,                  0,              0},
 };
 
 static void
 Usage()
 {
     printf("Usage: dropstats [--help]\n");
-    printf("Usage: dropstats [--core|-c] <core number>\n\n");
+    printf("Usage: dropstats [--core|-c] <core number> %s\n\n",
+            get_offload_enabled()?"[--offload|-o]":"");
     printf("--core <core number>\t Show statistics for a specified CPU core\n");
+    if (get_offload_enabled()) {
+        printf("--offload\t\t Show statistics for pkts offloaded on NIC\n");
+        printf("\t\t\t (offload stats included if no flags given)\n");
+    }
     exit(-EINVAL);
 }
 
@@ -113,7 +203,13 @@ parse_long_opts(int opt_index, char *opt_arg)
             Usage();
         }
         break;
-
+    case OFFL_OPT_INDEX:
+        if (!get_offload_enabled()) {
+            printf("Error: hardware offloads not enabled\n");
+            Usage();
+        }
+        core = -2;
+        break;
     case HELP_OPT_INDEX:
     default:
         Usage();
@@ -130,12 +226,19 @@ main(int argc, char *argv[])
 
     dropstats_fill_nl_callbacks();
 
-    while (((opt = getopt_long(argc, argv, "h:c:",
+    parse_ini_file();
+
+    while (((opt = getopt_long(argc, argv, "h:c:o",
                         long_options, &option_index)) >= 0)) {
         switch (opt) {
         case 'c':
             core_set = 1;
             parse_long_opts(CORE_OPT_INDEX, optarg);
+            break;
+
+        case 'o':
+            offload_set = 1;
+            parse_long_opts(OFFL_OPT_INDEX, optarg);
             break;
 
         case 0:
