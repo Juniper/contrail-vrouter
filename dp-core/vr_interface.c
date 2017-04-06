f39f54f2db6a90f9eadb01d8decd46111db99e96
Divakar D <ddivakar@juniper.net>
2017-04-06 10:33:18 +0530

    Cleanup SubVIF properly if addition fails
    On a given physical interface there can only be one subinterface per
    given Vlan ID. If Agent attempts to add multiple subinterfaces with same
    Valn ID, vrouter rejects that addition with -EEXIST error. As part of
    this rejection, the temproary subinterface need to be properly
    cleaned up. Right now the clean up is not proper w.r.t memory and the
    refcount of the physical interface is lso not handled well. This is
    leading to either memory leak/corruption.
    
    Also currently the ref counting of VIF is not symetric w.r.t increment
    and decrement. Decrement of ref count is done in atomic way but not
    increment. This as such can lead to issues when RCU callbacks
    of NH (which eventually decrement VIF ref count in atomic manner) are
    executed in parallel to new NH addition from Agent (which increments VIF
    ref count non atomic manner).
    
    As a fix, sub VIF is cleaned up properly and VIF ref count is
    incremented in atomic manner
    
    closes-bug: #1677571
    
    Conflicts:
    	dp-core/vr_interface.c
    
    Change-Id: I2a9d41f2b0152f700e2c5c96c9f39825c22ce482

--- dp-core/vr_interface.c
+++ dp-core/vr_interface.c
@@ -767,6 +767,7 @@ drop:
 static int
 vlan_drv_del(struct vr_interface *vif)
 {
+    int ret = 0;
     struct vr_interface *pvif;
 
     pvif = vif->vif_parent;
@@ -774,9 +775,14 @@ vlan_drv_del(struct vr_interface *vif)
         return 0;
 
     if (pvif->vif_driver->drv_delete_sub_interface)
-        pvif->vif_driver->drv_delete_sub_interface(pvif, vif);
+        ret = pvif->vif_driver->drv_delete_sub_interface(pvif, vif);
 
-    return 0;
+    if (vif->vif_src_mac) {
+        vr_free(vif->vif_src_mac);
+        vif->vif_src_mac = NULL;
+    }
+
+    return ret;
 }
 
 static int
@@ -1114,11 +1120,11 @@ eth_drv_del_sub_interface(struct vr_interface *pvif, struct vr_interface *vif)
         }
     }
 
-    if (!ret) {
-        vrouter_put_interface(pvif);
+    if (vif->vif_parent == pvif) {
         vif->vif_parent = NULL;
-        hif_ops->hif_del(vif);
+        vrouter_put_interface(pvif);
     }
+    hif_ops->hif_del(vif);
 
     return ret;
 }
@@ -1348,7 +1354,7 @@ vrouter_get_interface(unsigned int rid, unsigned int idx)
 
     vif = __vrouter_get_interface(router, idx);
     if (vif)
-        vif->vif_users++;
+        (void)__sync_add_and_fetch(&vif->vif_users, 1);
 
     return vif;
 }
