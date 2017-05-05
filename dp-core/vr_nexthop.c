3e274254f25563c3fe8ed564c9d3ef77880b7dcd
Anand H. Krishnan <anandhk@juniper.net>
2017-05-05 15:00:18 +0530

    A cloned skbuff should not be sent to napi_gro_receive
    When skb_gro_receive merges skbs/fragments, it does not take
    a reference to the underlying page, thus transferring the
    original reference it had to the first packet of the packet
    bundle. This will work fine for non-cloned packets.
    
    However, for cloned packets, when the gro-ed packet is
    eventually freed (because the original skb was not cloned and
    hence reference was 1), the merged skb's frags also get freed
    (put_page-ed) without taking into account the other references
    that were held for the fragments (dataref). This will result in
    bad page problems.
    
    Hence, avoid GRO for cloned packets.
    
    Change-Id: Ief768873bf4ea57bd88e3f809392bec92fc41932
    Closes-Bug: #1683305

--- dp-core/vr_nexthop.c
+++ dp-core/vr_nexthop.c
@@ -2439,8 +2439,9 @@ nh_encap_l2(struct vr_packet *pkt, struct vr_nexthop *nh,
 
     /* No GRO for multicast and user packets */
     if ((pkt->vp_flags & VP_FLAG_MULTICAST) ||
-            (fmd->fmd_vlan != VLAN_ID_INVALID))
-        pkt->vp_flags &= ~VP_FLAG_GRO;
+            (fmd->fmd_vlan != VLAN_ID_INVALID)) {
+        vr_pkt_unset_gro(pkt);
+    }
 
     vif = nh->nh_dev;
     if (!vif) {
@@ -2481,7 +2482,7 @@ nh_encap_l2(struct vr_packet *pkt, struct vr_nexthop *nh,
         }
     }
 
-    if ((pkt->vp_flags & VP_FLAG_GRO) && vif_is_virtual(vif) &&
+    if (vr_pkt_is_gro(pkt) && vif_is_virtual(vif) &&
             (!(vif->vif_flags & VIF_FLAG_MIRROR_TX))) {
         if (vr_gro_input(pkt, nh)) {
             if (stats)
@@ -2559,24 +2560,19 @@ nh_encap_l3(struct vr_packet *pkt, struct vr_nexthop *nh,
     }
 
     if (vr_pkt_is_diag(pkt)) {
-        pkt->vp_flags &= ~VP_FLAG_GRO;
+        vr_pkt_unset_gro(pkt);
         if (stats)
             stats->vrf_diags++;
-    } else {
-        if (stats) {
-            if ((pkt->vp_flags & VP_FLAG_GRO) &&
-                    vif_is_virtual(vif)) {
-                stats->vrf_gros++;
-            } else {
-                stats->vrf_encaps++;
-            }
-        }
     }
 
-    if ((pkt->vp_flags & VP_FLAG_GRO) && vif_is_virtual(vif) &&
+    if (vr_pkt_is_gro(pkt) && vif_is_virtual(vif) &&
             (!(vif->vif_flags & VIF_FLAG_MIRROR_TX))) {
-        if (vr_gro_input(pkt, nh))
+        if (vr_gro_input(pkt, nh)) {
+            if (stats) {
+                stats->vrf_gros++;
+            }
             return NH_PROCESSING_COMPLETE;
+        }
     }
 
     rewrite_len = vif->vif_set_rewrite(vif, pkt, fmd, nh->nh_data, nh->nh_encap_len);
@@ -2601,6 +2597,10 @@ nh_encap_l3(struct vr_packet *pkt, struct vr_nexthop *nh,
         vr_pset_data(pkt, pkt->vp_data);
         vr_trap(pkt, fmd->fmd_dvrf, AGENT_TRAP_DIAG, &vif->vif_idx);
         return NH_PROCESSING_COMPLETE;
+    }
+
+    if (stats) {
+        stats->vrf_encaps++;
     }
 
     vif->vif_tx(vif, pkt, fmd);
