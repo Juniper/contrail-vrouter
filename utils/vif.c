63cc1bf2b253dab26a2e7c669754dfbd2d73d2a9
Antoine Eiche <antoine.eiche@cloudwatt.com>
2017-10-25 22:05:46 +0200

    rt: Add monitor option
    rt --monitor can be used to get route creations and deletions in
    live. They are broadcasted on Netlink by the vrouter kernel module.
    Only routes of type AF_INET and AF_INET6 are fully printed while
    routes of type AF_BRIDGE are not generated yet.
    
    The output format is 'jsonline' to be easily parsed by external
    tools.
    
    This is an example of the output:
    {"operation":"add","family":"AF_INET","vrf_id":2,"prefix":32,"address":"20.1.1.254","nh":12,"flags":{"label_valid":false, "arp_proxy":true, "arp_trap":true, "arp_flood":false}}
    
    Change-Id: I8cb8556f1c4adda0bb0ef10f98fc38702b2942c4
    Closes-bug: #1650316

--- utils/vif.c
+++ utils/vif.c
@@ -964,7 +964,7 @@ op_retry:
         return ret;
 
 
-    ret = vr_recvmsg(cl, dump);
+    ret = vr_recvmsg(cl, dump, false);
     if (ret <= 0)
         return ret;
 
@@ -973,7 +973,7 @@ op_retry:
         if (ret < 0)
             return ret;
 
-        return vr_recvmsg(cl, dump);
+        return vr_recvmsg(cl, dump, false);
     }
 
     if (dump_pending) {
