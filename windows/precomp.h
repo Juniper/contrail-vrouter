2836d1bf20e502d9b9e0e2502c18213f23be5f30
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2017-12-01 13:23:09 +0100

    vrouter: vcxproj files for building vRouter as Hyper-V Extension
    Changes required to support Contrail on Windows:
    - VS 2015 project files required to build vRouter as Hyper-V Extension
    
    Initial work:
      https://github.com/codilime/contrail-vrouter/commits/windows
    
    Change-Id: Idabf2caa4d37331f2502b1d9b8b686be4a34d80f
    Partial-Bug: #1734699

---
+++ windows/precomp.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <ndis.h>
+#include <netiodef.h>
+#include <intsafe.h>
+#include <ntintsafe.h>
+#include <Ntstrsafe.h>
+#include <wdmsec.h>
+
+#include "vr_windows.h"
+#include "windows_mem.h"
+#include "windows_devices.h"
