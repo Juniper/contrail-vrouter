231f4a4353b6e6a05f2f95b198e600b012649222
Kiran Kasim <kirankn@juniper.net>
2018-04-02 10:18:43 -0700

    DPDK: Add DPDK version to the log
    Add DPDK library version in the log
    
    Change-Id: I049ef94704b6d909bb2e1f6636416c1c123eb9c8
    Closes-Bug: 1760125

--- dpdk/dpdk_vrouter.c
+++ dpdk/dpdk_vrouter.c
@@ -30,6 +30,7 @@
 #include "vr_mem.h"
 #include "nl_util.h"
 
+#include <rte_version.h>
 #include <rte_errno.h>
 #include <rte_ethdev.h>
 #include <rte_kni.h>
@@ -605,7 +606,8 @@ dpdk_argv_update(void)
 static void
 version_print(void)
 {
-    RTE_LOG(INFO, VROUTER, "vRouter/DPDK version: %s\n", ContrailBuildInfo);
+    RTE_LOG(INFO, VROUTER, "vRouter version: %s\n", ContrailBuildInfo);
+    RTE_LOG(INFO, VROUTER, "DPDK version: %s\n", rte_version());
 }
 
 /*
