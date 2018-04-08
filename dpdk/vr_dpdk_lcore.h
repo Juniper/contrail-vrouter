ddf0ae09fe200816ef407b791565335a2f73a47e
e-kuznetsov <evgeny.kuznetsov@outlook.com>
2018-04-08 19:36:47 +0300

    Fixing copyrights
    Change-Id: I54777405fefb39420744f2c0cc2f2a3a469aec0d
    Partial-Bug: #1761692
    (cherry picked from commit 7bcecddb8149709162f9ae3dbd7be67818ff4c82)

--- dpdk/vr_dpdk_lcore.h
+++ dpdk/vr_dpdk_lcore.h
@@ -2,7 +2,7 @@
  * vr_dpdk_lcore.h - header for DPDK lcore support functions.
  *
  *
- * Copyright (c) 2015 Semihalf.
+ * Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
  */
 
 #ifndef __VR_DPDK_LCORE_H__
