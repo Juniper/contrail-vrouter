185f1b25e2d7fef108f692d428f01cc493b94b1b
Michał Krasnoborski <michal.krasnoborski@codilime.com>
2018-06-14 14:10:33 +0200

    Refactor NBL and vr_packet memory management in windows vrouter
    This refactor is required in order to fix IP Fragmentation support
    in Windows vrouter extension.
    
    Refactor memory management of Net Buffer Lists and vr_packets:
    * Net Buffer Lists no longer store vr_packet in Context,
    * vr_packets may only point to leaf Net Buffer Lists,
    * vr_packets are deallocated before NdisFSendNetBufferLists.
    * Parent Net Buffer Lists are deallocated based on atomic ChildRefCount.
    
    Change-Id: I84d01e68f038f3f9ffc47ce7da47c2824ff4ff87
    Partial-Bug: #1773203
    (cherry picked from commit 7968392eeaaea036a0c5edc1b27112d25019c006)

--- test/windows/kernel_impl/include/ntddk.h
+++ test/windows/kernel_impl/include/ntddk.h
@@ -4,6 +4,10 @@
 
 #pragma once
 
-#include <assert.h>
+extern void mock_assert(const int result, const char* const expression,
+                        const char * const file, const int line);
 
-#define ASSERT assert
+#define ASSERT(expression) \
+    mock_assert((int)(expression), #expression, __FILE__, __LINE__)
+#define ASSERTMSG(_Msg, _Expr) ASSERT((_Expr) && (_Msg))
+#define assert ASSERT
