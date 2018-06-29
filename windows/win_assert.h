9466a27f0e4c413456d7b8d85b1806c9cd874452
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2018-06-29 12:37:12 +0200

    WinPacketFree unit testing
    Change-Id: I349da7e9f002de1a2c47c1f1ff632239d80ffbfe
    Partial-Bug: #1737177

---
+++ windows/win_assert.h
@@ -0,0 +1,20 @@
+/*
+ * win_assert.h
+ *
+ * Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+ */
+#ifndef _WIN_ASSERT_H_
+#define _WIN_ASSERT_H_
+
+#ifdef __KERNEL__
+#include <ntddk.h>
+#define WinAssert(expression) ASSERT(expression)
+#else
+extern void mock_assert(const int result, const char* const expression,
+                        const char * const file, const int line);
+
+#define WinAssert(expression) \
+    mock_assert((int)(expression), #expression, __FILE__, __LINE__);
+#endif
+
+#endif // _WIN_ASSERT_H_
