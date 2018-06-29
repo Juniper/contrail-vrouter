4e335e7c3f43960f3fbcf01b0830e8ee61126829
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2018-06-29 12:37:12 +0200

    [DO NOT MERGE] WinPacketFree unit testing
    Change-Id: I349da7e9f002de1a2c47c1f1ff632239d80ffbfe
    Partial-Bug: #1737177

--- test/windows/fake_win_packet_raw.c
+++ test/windows/fake_win_packet_raw.c
@@ -13,6 +13,7 @@
 struct _WIN_PACKET {
     PWIN_PACKET Parent;
     LONG ChildRefCount;
+    BOOL IsOwned;
 };
 
 PWIN_PACKET
@@ -54,9 +55,53 @@ WinPacketRawIncrementChildCountOf(PWIN_PACKET Packet)
     Packet->ChildRefCount++;
 }
 
+LONG
+WinPacketRawDecrementChildCountOf(PWIN_PACKET Packet)
+{
+    return --Packet->ChildRefCount;
+}
+
 static PWIN_PACKET
 WinPacketRawAllocateClone_Impl(PWIN_PACKET Packet)
 {
-    return NULL;
+    return Fake_WinPacketAllocate();
 }
 PWIN_PACKET (*WinPacketRawAllocateClone)(PWIN_PACKET Packet) = WinPacketRawAllocateClone_Impl;
+
+VOID
+WinPacketRawFreeClone(PWIN_PACKET Packet)
+{
+    Fake_WinPacketFree(Packet);
+}
+
+BOOL
+WinPacketRawIsOwned(PWIN_PACKET Packet)
+{
+    return Packet->IsOwned;
+}
+
+BOOL
+WinPacketRawIsCloned(PWIN_PACKET Packet)
+{
+    return Packet->Parent != NULL;
+}
+
+VOID
+Fake_WinPacketSetIsOwned(PWIN_PACKET Packet, BOOL IsOwned)
+{
+    Packet->IsOwned = IsOwned;
+}
+
+static VOID
+WinPacketRawComplete_Impl(PWIN_PACKET Packet)
+{
+    Fake_WinPacketFree(Packet);
+}
+VOID (*WinPacketRawComplete)(PWIN_PACKET Packet) = WinPacketRawComplete_Impl;
+
+static VOID
+WinPacketRawFreeCreated_Impl(PWIN_PACKET Packet)
+{
+    Fake_WinPacketFree(Packet);
+}
+VOID (*WinPacketRawFreeCreated)(PWIN_PACKET Packet) = WinPacketRawFreeCreated_Impl;
