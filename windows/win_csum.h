e5e321335c35a2ff762b62cf413d34f4ddd9dd29
Michał Krasnoborski <michal.krasnoborski@codilime.com>
2018-08-30 17:19:59 +0200

    Handle LSO in windows vrouter
    Change-Id: I886f6582037ce8c404c546b16d126dffd394504d
    Partial-Bug: #1737177
    Co-authored-by: Michal Krasnoborski <michal.krasnoborski@codilime.com>
    Co-authored-by: Mateusz Jablonski <mateusz.jablonski@codilime.com>
    Co-authored-by: Jacek Izykowski <jacek.izykowski@codilime.com>

---
+++ windows/win_csum.h
@@ -0,0 +1,33 @@
+/*
+ * win_csum.h -- checksum calculation utilities
+ *
+ * Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+ */
+#ifndef __WIN_CSUM_H__
+#define __WIN_CSUM_H__
+
+#include "vr_os.h"
+#include "vr_packet.h"
+
+__attribute__packed__open__
+struct tcp_pseudo_header {
+    unsigned int source_address;
+    unsigned int destination_address;
+    unsigned char reserved;
+    unsigned char protocol;
+    unsigned short tcp_length;
+} __attribute__packed__close__;
+
+uint16_t trim_csum(uint32_t csum);
+uint16_t calc_csum(uint8_t* ptr, size_t size);
+
+void init_tcp_pseudo_header(
+    struct vr_ip* ip_hdr,
+    struct tcp_pseudo_header* tcp_pseudo_hdr);
+void fill_partial_csum_of_tcp_packet(
+    struct vr_ip* ip_header,
+    struct vr_tcp* tcp_header);
+void fill_csum_of_tcp_packet_provided_that_partial_csum_is_computed(
+    uint8_t* ip_packet);
+
+#endif //__WIN_CSUM_H__
