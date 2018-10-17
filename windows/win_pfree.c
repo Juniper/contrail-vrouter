d9bc6b0fbee9356c6819b571d61d3bb8f7464d4a
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2018-10-17 17:44:31 +0200

    [DO NOT MERGE] Windows: Fragmentation test
    Change-Id: Ic5e2bd01c6a4a1f6a64a02c49c8be690a05a4ccd
    Co-authored-by: Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
    Co-authored-by: Mateusz Jablonski <mateusz.jablonski@codilime.com>
    Partial-Bug: #1796663

--- windows/win_pfree.c
+++ windows/win_pfree.c
@@ -3,9 +3,11 @@
  */
 
 #include "vr_packet.h"
-#include "win_packet.h"
 #include "win_assert.h"
 #include "win_memory.h"
+#include "win_packet_impl.h"
+#include "win_packet_raw.h"
+#include "win_packet.h"
 
 // TODO: This function is defined in:
 //       - for unit tests - fake_vrouter.c
@@ -22,7 +24,13 @@ win_pfree(struct vr_packet *pkt, unsigned short reason)
     win_update_drop_stats(pkt, reason);
 
     PVR_PACKET_WRAPPER wrapper = GetWrapperFromVrPacket(pkt);
-    WinPacketFreeRecursive(wrapper->WinPacket);
+    PWIN_PACKET winPacket = wrapper->WinPacket;
+    PWIN_PACKET_RAW rawPacket = WinPacketToRawPacket(winPacket);
+
+    // Assumption: vr_packet points to leaf NBL. Please refer to
+    // windows/vr_nbl.c for vr_packet - NBL relationship description.
+    WinAssert(WinPacketRawGetChildCountOf(rawPacket) == 0);
+    WinPacketFreeRecursive(winPacket);
 
     WinRawFree(wrapper);
 }
