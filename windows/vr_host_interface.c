d3319be565d771251ee7938019f0c9b4b033a240
Jacek Chalupka <jacek.chalupka@codilime.com>
2018-10-31 16:55:44 +0100

    Moved Windows TX Postprocess
    Change-Id: I1a71cfceba1391f4b61dd85e3365a26853d22b05
    Partial-Bug: #1796663

--- windows/vr_host_interface.c
+++ windows/vr_host_interface.c
@@ -16,6 +16,7 @@
 #include "win_packet_splitting.h"
 #include "windows_devices.h"
 #include "windows_nbl.h"
+#include "win_tx_postprocess.h"
 
 static BOOLEAN physicalVifAdded;
 
@@ -79,162 +80,6 @@ win_if_del_tap(struct vr_interface *vif)
     return 0;
 }
 
-// TODO: To be removed after WinTxPostprocess() went live.
-static void
-fix_ip_csum_at_offset(struct vr_packet *pkt, unsigned offset)
-{
-    struct vr_ip *iph;
-
-    ASSERT(0 < offset);
-
-    iph = (struct vr_ip *)(pkt_data(pkt) + offset);
-    iph->ip_csum = vr_ip_csum(iph);
-}
-
-// TODO: To be removed after WinTxPostprocess() went live.
-static void
-zero_ip_csum_at_offset(struct vr_packet *pkt, unsigned offset)
-{
-    struct vr_ip *iph;
-
-    ASSERT(0 < offset);
-
-    iph = (struct vr_ip *)(pkt_data(pkt) + offset);
-    iph->ip_csum = 0;
-}
-
-// TODO: To be removed after WinTxPostprocess() went live.
-static bool fix_csum(struct vr_packet *pkt, unsigned offset)
-{
-    uint32_t csum;
-    uint16_t size;
-    uint8_t type;
-
-    PWIN_PACKET winPacket = GetWinPacketFromVrPacket(pkt);
-    PWIN_PACKET_RAW winPacketRaw = WinPacketToRawPacket(winPacket);
-    PNET_BUFFER_LIST nbl = WinPacketRawToNBL(winPacketRaw);
-    PNET_BUFFER nb = NET_BUFFER_LIST_FIRST_NB(nbl);
-
-    void* packet_data_buffer = ExAllocatePoolWithTag(NonPagedPoolNx, NET_BUFFER_DATA_LENGTH(nb), VrAllocationTag);
-
-    // Copy the packet. This function will not fail if ExAllocatePoolWithTag succeeded
-    // So no need to clean it up
-    // If ExAllocatePoolWithTag failed (packet_data_buffer== NULL),
-    // this function will work okay if the data is contigous.
-    uint8_t* packet_data = NdisGetDataBuffer(nb, NET_BUFFER_DATA_LENGTH(nb), packet_data_buffer, 1, 0);
-
-    if (packet_data == NULL)
-        // No need for free
-        return false;
-
-    if (pkt->vp_type == VP_TYPE_IP6 || pkt->vp_type == VP_TYPE_IP6OIP) {
-        struct vr_ip6 *hdr = (struct vr_ip6*) (packet_data + offset);
-        offset += sizeof(struct vr_ip6);
-        size = ntohs(hdr->PayloadLength);
-
-        type = hdr->NextHeader;
-    } else {
-        struct vr_ip *hdr = (struct vr_ip*) &packet_data[offset];
-        offset += hdr->ip_hl * 4;
-        size = ntohs(hdr->ip_len) - 4 * hdr->ip_hl;
-
-        type = hdr->ip_proto;
-    }
-
-    uint8_t* payload = &packet_data[offset];
-    csum = calc_csum((uint8_t*) payload, size);
-
-    // This time it's the "real" packet. Header being contiguous is guaranteed, but nothing else
-    if (type == VR_IP_PROTO_UDP) {
-        struct vr_udp* udp = (struct vr_udp*) pkt_data_at_offset(pkt, offset);
-        udp->udp_csum = htons(~(trim_csum(csum)));
-    } else if (type == VR_IP_PROTO_TCP) {
-        struct vr_tcp* tcp = (struct vr_tcp*) pkt_data_at_offset(pkt, offset);
-        tcp->tcp_csum = htons(~(trim_csum(csum)));
-    }
-
-    if (packet_data_buffer)
-        ExFreePool(packet_data_buffer);
-
-    return true;
-}
-
-// TODO: To be removed after WinTxPostprocess() went live.
-static void
-fix_tunneled_csum(struct vr_packet *pkt)
-{
-    PWIN_PACKET winPacket = GetWinPacketFromVrPacket(pkt);
-    PWIN_PACKET_RAW winPacketRaw = WinPacketToRawPacket(winPacket);
-    PNET_BUFFER_LIST nbl = WinPacketRawToNBL(winPacketRaw);
-    NDIS_TCP_IP_CHECKSUM_NET_BUFFER_LIST_INFO settings;
-    settings.Value = NET_BUFFER_LIST_INFO(nbl, TcpIpChecksumNetBufferListInfo);
-
-    if (settings.Transmit.IpHeaderChecksum) {
-        // Zero the outer checksum, it'll be offloaded
-        zero_ip_csum_at_offset(pkt, sizeof(struct vr_eth));
-        // Fix the inner checksum, it will not be offloaded
-        fix_ip_csum_at_offset(pkt, pkt->vp_inner_network_h);
-    } else {
-        // Fix the outer checksum
-        fix_ip_csum_at_offset(pkt, sizeof(struct vr_eth));
-        // Inner checksum is OK
-    }
-
-    if (settings.Transmit.TcpChecksum) {
-        // Calculate the header/data csum and turn off HW acceleration
-        if (fix_csum(pkt, pkt->vp_inner_network_h)) {
-            settings.Transmit.TcpChecksum = 0;
-            settings.Transmit.TcpHeaderOffset = 0;
-            NET_BUFFER_LIST_INFO(nbl, TcpIpChecksumNetBufferListInfo) = settings.Value;
-        }
-        // else try to offload it even though it's tunneled.
-    }
-
-    if (settings.Transmit.UdpChecksum) {
-        // Calculate the header/data csum and turn off HW acceleration
-        if (fix_csum(pkt, pkt->vp_inner_network_h)) {
-            settings.Transmit.UdpChecksum = 0;
-            NET_BUFFER_LIST_INFO(nbl, TcpIpChecksumNetBufferListInfo) = settings.Value;
-        }
-        // else try to offload it even though it's tunneled.
-    }
-}
-
-// Fixes the IPv4 checksum and sets the correct info.Transmit value.
-//
-// TODO try to offload the checksums when doing fragmentation too.
-// TODO: To be removed after WinTxPostprocess() went live.
-static void
-fix_ip_v4_csum(struct vr_packet *pkt) {
-    PWIN_PACKET winPacket = GetWinPacketFromVrPacket(pkt);
-    PWIN_PACKET_RAW winPacketRaw = WinPacketToRawPacket(winPacket);
-    PNET_BUFFER_LIST nbl = WinPacketRawToNBL(winPacketRaw);
-    NDIS_TCP_IP_CHECKSUM_NET_BUFFER_LIST_INFO settings;
-    settings.Value = NET_BUFFER_LIST_INFO(nbl, TcpIpChecksumNetBufferListInfo);
-
-    if (pkt->vp_data != 0) {
-        // This packet came to us in a tunnel (which is now unwrapped),
-        // therefore .Receive is now the active field of the settings union
-        // (although we don't look at it as it refers to the outer headers anyway).
-        // We assume all the checksums are valid, so we need to explicitly disable
-        // offloading (so the .Receive field isn't erroneously reinterpreted as .Transmit)
-        NET_BUFFER_LIST_INFO(nbl, TcpIpChecksumNetBufferListInfo) = 0;
-    } else {
-        // This packet comes from a container or the agent,
-        // therefore we should look at settings.Transmit.
-        if (settings.Transmit.IpHeaderChecksum) {
-            // If computation of IP checksum is about to be offloaded, its value
-            // should be set to zero (because initial checksum's value is taken into
-            // account when computing the checksum). However, dp-core doesn't care about
-            // this specific case (e.g. vr_incremental_diff/vr_ip_incremental_csum are
-            // called to incrementally "improve" checksum).
-            zero_ip_csum_at_offset(pkt, sizeof(struct vr_eth));
-        } else {
-            // No offloading requested - checksum should be valid.
-        }
-    }
-}
-
 static NDIS_SWITCH_PORT_DESTINATION
 VrInterfaceToDestination(struct vr_interface *vif)
 {
@@ -258,20 +103,8 @@ MarkNetBufferListAsSafe(PNET_BUFFER_LIST NetBufferList)
 static int
 __win_if_tx(struct vr_interface *vif, struct vr_packet *pkt)
 {
-    if (vr_pkt_type_is_overlay(pkt->vp_type)) {
-        fix_tunneled_csum(pkt);
-    } else if (pkt->vp_type == VP_TYPE_IP) {
-        // There's no checksum in IPv6 header.
-        fix_ip_v4_csum(pkt);
-    }
-
-    PWIN_PACKET winPacket = GetWinPacketFromVrPacket(pkt);
-    PWIN_PACKET_RAW winPacketRaw = WinPacketToRawPacket(winPacket);
-
-    PWIN_MULTI_PACKET fragmentedWinPacket = split_packet_if_needed(pkt);
-    if (fragmentedWinPacket != NULL) {
-        winPacketRaw = WinMultiPacketToRawPacket(fragmentedWinPacket);
-    }
+    PWIN_MULTI_PACKET postprocessedWinPacket = WinTxPostprocess(pkt);
+    PWIN_PACKET_RAW winPacketRaw = WinMultiPacketToRawPacket(postprocessedWinPacket);
 
     PNET_BUFFER_LIST nbl = WinPacketRawToNBL(winPacketRaw);
 
