6565435e7deef201e60538d2e9ddc1b145a54254
hajim <hajim@juniper.net>
2018-11-29 15:43:22 +0530

    vrouter pkt drop log buffer infra. This feature is used to log additional information when a packet is dropeed in vrouter module. To run pkt drop log : dropstats -log <core number> If core number specified as 0, it will log for all cores. Currently below features are enabled.  - Log pkt drops info for all cores  - provided support to enable/disable pkt drop log during load time and    compile    time.  - Provided support to configure MAX. number of records during load time    and compile time.
    Change-Id: I9b19c22b86a6f604e1bebf5d2e59e173ee5d88cf
    Partial-Bug: 1728010

--- dpdk/dpdk_vrouter.c
+++ dpdk/dpdk_vrouter.c
@@ -91,6 +91,8 @@ enum vr_opt_index {
     NETLINK_PORT_OPT_INDEX,
 #define SOCKET_MEM_OPT          "socket-mem"
     SOCKET_MEM_OPT_INDEX,
+#define PKT_DROP_LOG_BUFFER_SIZE_OPT "vr_dropstats_bufsz"
+    PKT_DROP_LOG_BUFFER_SIZE_OPT_INDEX,
 #define LCORES_OPT              "lcores"
     LCORES_OPT_INDEX,
 #define MEMORY_ALLOC_CHECKS_OPT "vr_memory_alloc_checks"
@@ -104,6 +106,7 @@ extern unsigned int vr_bridge_oentries;
 extern unsigned int vr_mpls_labels;
 extern unsigned int vr_nexthops;
 extern unsigned int vr_vrfs;
+extern unsigned int vr_pkt_droplog_bufsz;
 
 static int no_daemon_set;
 static int no_gro_set = 0;
@@ -630,6 +633,8 @@ dpdk_argv_update(void)
                 vr_rxd_sz);
     RTE_LOG(INFO, VROUTER, "Maximum packet size:         %" PRIu32 "\n",
                 vr_packet_sz);
+    RTE_LOG(INFO, VROUTER, "Maximum log buffer size:     %" PRIu32 "\n",
+		vr_pkt_droplog_bufsz);
     RTE_LOG(INFO, VROUTER, "EAL arguments:\n");
     for (i = 1; i < RTE_DIM(dpdk_argv) - 1; i += 2) {
         if (dpdk_argv[i] == NULL)
@@ -970,6 +975,8 @@ static struct option long_options[] = {
                                                     NULL,                   0},
     [SOCKET_MEM_OPT_INDEX]          =   {SOCKET_MEM_OPT,        required_argument,
                                                     NULL,                   0},
+    [PKT_DROP_LOG_BUFFER_SIZE_OPT_INDEX] =   {PKT_DROP_LOG_BUFFER_SIZE_OPT, required_argument,
+                                                    NULL,                   0},
     [MEMORY_ALLOC_CHECKS_OPT_INDEX] =   {MEMORY_ALLOC_CHECKS_OPT, no_argument,
                                                     NULL,                   0},
     [MAX_OPT_INDEX]                 =   {NULL,                  0,
@@ -1009,6 +1016,7 @@ Usage(void)
         "    --"DPDK_TXD_SIZE_OPT" NUM    DPDK PMD Tx Descriptor size\n"
         "    --"DPDK_RXD_SIZE_OPT" NUM    DPDK PMD Rx Descriptor size\n"
         "    --"PACKET_SIZE_OPT" NUM      Maximum packet size\n"
+	"    --"PKT_DROP_LOG_BUFFER_SIZE_OPT" NUM Maximum debug log buffer size\n"
         );
 
     exit(1);
@@ -1150,6 +1158,12 @@ parse_long_opts(int opt_flow_index, char *optarg)
         vr_vrfs = (unsigned int)strtoul(optarg, NULL, 0);
         if (errno != 0) {
             vr_vrfs = VR_DEF_VRFS;
+        }
+        break;
+    case PKT_DROP_LOG_BUFFER_SIZE_OPT_INDEX:
+        vr_pkt_droplog_bufsz = (unsigned int)strtoul(optarg, NULL, 0);
+        if (errno != 0) {
+            vr_pkt_droplog_bufsz = VR_PKT_DROP_LOG_MAX;
         }
         break;
 
