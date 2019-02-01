37b4a93537d9a763649e49b3bf726735c7b07430
Piotr Szuberski <piotr.szuberski@codilime.com>
2019-02-01 16:55:36 +0100

    Refactored ksync
    1. Extracted some funtions from vr_ksync.c to two files to make
       it possible to test bulk-message support.
       vr_ksync_kernel stands for functions that can't be directly
       called from userspace.
       vr_ksync_user functions can be called from userspace.
    2. KsyncParseAndHandleWrite is independent of IRP structure part
       of KsyncDispatchWrite function.
    3. Wrapped system PrintDbg function.
    4. Added wrappers of few memory functions.
    
    Partial Jira Bug: WIND-1
    
    Change-Id: Iffd1993f897f77111836c4128b537cf7d4514352

--- windows/vr_ksync.c
+++ windows/vr_ksync.c
@@ -6,35 +6,13 @@
 
 #include "vr_genetlink.h"
 #include "vr_message.h"
-
-#define NLA_DATA(nla)   ((char *)nla + NLA_HDRLEN)
-#define NLA_LEN(nla)    (nla->nla_len - NLA_HDRLEN)
-
-static ULONG KsyncAllocationTag = 'NYSK';
+#include "vr_ksync_user.h"
 
 const WCHAR KsyncDeviceName[]    = L"\\Device\\vrouterKsync";
 const WCHAR KsyncDeviceSymLink[] = L"\\DosDevices\\vrouterKsync";
 
 static PDEVICE_OBJECT KsyncDeviceObject   = NULL;
 static NDIS_HANDLE    KsyncDeviceHandle   = NULL;
-
-static PKSYNC_DEVICE_CONTEXT
-KSyncAllocContext()
-{
-    PKSYNC_DEVICE_CONTEXT ctx = ExAllocatePoolWithTag(NonPagedPoolNx,
-                                                      sizeof(*ctx),
-                                                      KsyncAllocationTag);
-    if (ctx == NULL)
-        return NULL;
-
-    ctx->WrittenBytes = 0;
-    ctx->WriteBufferSize = sizeof(ctx->WriteBuffer);
-    RtlZeroMemory(ctx->WriteBuffer, ctx->WriteBufferSize);
-
-    ctx->responses = NULL;
-
-    return ctx;
-}
 
 static void
 KsyncAttachContextToFileContext(PKSYNC_DEVICE_CONTEXT ctx, PIRP irp)
@@ -52,39 +30,12 @@ KSyncGetContextFromFileContext(PIRP irp)
     return file_obj->FsContext;
 }
 
-static PKSYNC_RESPONSE
-KsyncResponseCreate()
-{
-    PKSYNC_RESPONSE resp;
-
-    resp = ExAllocatePoolWithTag(NonPagedPoolNx, sizeof(*resp), KsyncAllocationTag);
-    if (resp != NULL) {
-        RtlZeroMemory(resp, sizeof(*resp));
-    }
-
-    return resp;
-}
-
 static VOID
 KsyncResponseDelete(PKSYNC_RESPONSE resp)
 {
     ASSERT(resp != NULL);
 
     ExFreePool(resp);
-}
-
-static VOID
-KsyncAppendResponse(PKSYNC_DEVICE_CONTEXT ctx, PKSYNC_RESPONSE resp)
-{
-    PKSYNC_RESPONSE  elem;
-    PKSYNC_RESPONSE *iter = &(ctx->responses);
-
-    while (*iter != NULL) {
-        elem = *iter;
-        iter = &(elem->next);
-    }
-
-    *iter = resp;
 }
 
 static VOID
@@ -106,13 +57,6 @@ KsyncPopResponse(PKSYNC_DEVICE_CONTEXT ctx)
     } else {
         return NULL;
     }
-}
-
-static VOID
-KsyncContextResetWriteBuffer(PKSYNC_DEVICE_CONTEXT ctx)
-{
-    ctx->WrittenBytes = 0;
-    RtlZeroMemory(ctx->WriteBuffer, ctx->WriteBufferSize);
 }
 
 static inline NTSTATUS
@@ -158,107 +102,6 @@ KsyncDispatchCleanup(PDEVICE_OBJECT DeviceObject, PIRP Irp)
     return KSyncCompleteIrp(Irp, STATUS_SUCCESS, 0);
 }
 
-static NTSTATUS
-KsyncHandleWrite(PKSYNC_DEVICE_CONTEXT ctx, uint8_t *buffer, size_t buffer_size)
-{
-    struct vr_message request;
-    struct vr_message *response;
-    uint32_t multi_flag;
-    int ret;
-
-    /* Received buffer contains tightly packed Netlink headers, thus we can just
-       increment appropriate headers */
-    struct nlmsghdr   *nlh   = (struct nlmsghdr *)(buffer);
-    struct genlmsghdr *genlh = (struct genlmsghdr *)(nlh + 1);
-    struct nlattr     *nla   = (struct nlattr *)(genlh + 1);
-
-    request.vr_message_buf = NLA_DATA(nla);
-    request.vr_message_len = NLA_LEN(nla);
-
-    ret = vr_message_request(&request);
-    if (ret) {
-        if (vr_send_response(ret)) {
-            DbgPrint("%s: generating error response has failed\n", __func__);
-            return STATUS_INVALID_PARAMETER;
-        }
-    }
-
-    multi_flag = 0;
-    while ((response = vr_message_dequeue_response())) {
-        if (!multi_flag && !vr_response_queue_empty())
-            multi_flag = NLM_F_MULTI;
-
-        char *data = response->vr_message_buf - NETLINK_HEADER_LEN;
-        size_t data_len = NLMSG_ALIGN(response->vr_message_len + NETLINK_HEADER_LEN);
-
-        struct nlmsghdr *nlh_resp = (struct nlmsghdr *)(data);
-        nlh_resp->nlmsg_len = data_len;
-        nlh_resp->nlmsg_type = nlh->nlmsg_type;
-        nlh_resp->nlmsg_flags = multi_flag;
-        nlh_resp->nlmsg_seq = nlh->nlmsg_seq;
-		nlh_resp->nlmsg_pid = 0;
-
-        /* 'genlmsghdr' should be put directly after 'nlmsghdr', thus we can just
-           increment previous header pointer */
-        struct genlmsghdr *genlh_resp = (struct genlmsghdr *)(nlh_resp + 1);
-        RtlCopyMemory(genlh_resp, genlh, sizeof(*genlh_resp));
-
-        /* 'nlattr' should be put directly after 'genlmsghdr', thus we can just
-           increment previous header pointer */
-        struct nlattr *nla_resp = (struct nlattr *)(genlh_resp + 1);
-        nla_resp->nla_len = response->vr_message_len;
-        nla_resp->nla_type = NL_ATTR_VR_MESSAGE_PROTOCOL;
-
-        PKSYNC_RESPONSE ks_resp = KsyncResponseCreate();
-        if (ks_resp != NULL) {
-            ks_resp->message_len = data_len;
-            RtlCopyMemory(ks_resp->buffer, data, ks_resp->message_len);
-            KsyncAppendResponse(ctx, ks_resp);
-        } else {
-            DbgPrint("%s: ksync_response allocation failed\n", __func__);
-            return STATUS_INSUFFICIENT_RESOURCES;
-        }
-
-        vr_message_free(response);
-    }
-
-    if (multi_flag) {
-        PKSYNC_RESPONSE ks_resp = KsyncResponseCreate();
-        if (ks_resp == NULL) {
-            DbgPrint("%s: ksync_response allocation failed\n", __func__);
-            return STATUS_INSUFFICIENT_RESOURCES;
-        }
-
-        struct nlmsghdr *nlh_done = (struct nlmsghdr *)ks_resp->buffer;
-        nlh_done->nlmsg_len = NLMSG_HDRLEN;
-        nlh_done->nlmsg_type = NLMSG_DONE;
-        nlh_done->nlmsg_flags = 0;
-        nlh_done->nlmsg_seq = nlh->nlmsg_seq;
-        nlh_done->nlmsg_pid = 0;
-
-        ks_resp->message_len = NLMSG_HDRLEN;
-
-        KsyncAppendResponse(ctx, ks_resp);
-    }
-
-    return STATUS_SUCCESS;
-}
-
-static VOID
-KsyncCopyUserBufferToContext(PKSYNC_DEVICE_CONTEXT ctx,
-                             ULONG bytesNeeded,
-                             PCHAR *userBuffer,
-                             ULONG *userBufferSize,
-                             ULONG *writtenBytes)
-{
-    size_t bytesToWrite = *userBufferSize < bytesNeeded ? *userBufferSize : bytesNeeded;
-    RtlCopyMemory(&ctx->WriteBuffer[ctx->WrittenBytes], *userBuffer, bytesToWrite);
-    ctx->WrittenBytes += bytesToWrite;
-    *writtenBytes += bytesToWrite;
-    *userBufferSize -= bytesToWrite;
-    *userBuffer += bytesToWrite;
-}
-
 NTSTATUS
 KsyncDispatchWrite(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 {
@@ -277,34 +120,10 @@ KsyncDispatchWrite(PDEVICE_OBJECT DeviceObject, PIRP Irp)
         return KSyncCompleteIrp(Irp, STATUS_INSUFFICIENT_RESOURCES, 0);
     }
 
-    ULONG writtenBytes = 0;
-    while (userBufferSize > 0) {
-        if (ctx->WrittenBytes < sizeof(struct nlmsghdr)) {
-            size_t bytesNeeded = sizeof(struct nlmsghdr) - ctx->WrittenBytes;
-            KsyncCopyUserBufferToContext(ctx, bytesNeeded, &userBuffer, &userBufferSize, &writtenBytes);
-        } else {
-            struct nlmsghdr *nlh = (struct nlmsghdr *)ctx->WriteBuffer;
-            if (nlh->nlmsg_len > ctx->WriteBufferSize) {
-                KsyncContextResetWriteBuffer(ctx);
-                return KSyncCompleteIrp(Irp, STATUS_UNSUCCESSFUL, 0);
-            }
+    PairStatusInformation psi =
+            KsyncParseAndHandleWrite(ctx, userBuffer, userBufferSize);
 
-            size_t bytesNeeded = nlh->nlmsg_len - ctx->WrittenBytes;
-            KsyncCopyUserBufferToContext(ctx, bytesNeeded, &userBuffer, &userBufferSize, &writtenBytes);
-
-            if (ctx->WrittenBytes == nlh->nlmsg_len) {
-                NTSTATUS status = KsyncHandleWrite(ctx, ctx->WriteBuffer, ctx->WrittenBytes);
-                KsyncContextResetWriteBuffer(ctx);
-                if (NT_ERROR(status)) {
-                    DbgPrint("%s: KsyncHandleWrite returned an error: %x\n", __func__, status);
-                    return KSyncCompleteIrp(Irp, STATUS_UNSUCCESSFUL, writtenBytes);
-                }
-            }
-            break;
-        }
-    }
-
-    return KSyncCompleteIrp(Irp, STATUS_SUCCESS, writtenBytes);
+    return KSyncCompleteIrp(Irp, psi.Status, psi.Information);
 }
 
 NTSTATUS
---
+++ windows/vr_ksync_kernel.h
@@ -0,0 +1,34 @@
+/*
+ * vr_ksync_kernel.h -- ksync functions that need to be redefined to
+ *                      be appropriate for unit testing in user space
+ * 
+ * Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+ */
+#ifndef _VR_KSYNC_KERNEL_H_
+#define _VR_KSYNC_KERNEL_H_
+
+#include <windows_types.h>
+#include <windows_ksync.h>
+
+typedef struct _PairStatusInformation PairStatusInformation,
+                                      *PPairStatusInformation;
+
+struct _PairStatusInformation {
+    NTSTATUS Status;
+    ULONG Information;
+};
+
+extern ULONG KsyncAllocationTag;
+
+extern PKSYNC_RESPONSE
+KsyncResponseCreate();
+
+extern VOID
+KsyncAppendResponse(PKSYNC_DEVICE_CONTEXT ctx, PKSYNC_RESPONSE resp);
+
+NTSTATUS
+KsyncHandleWrite(PKSYNC_DEVICE_CONTEXT ctx,
+                 uint8_t *buffer,
+                 size_t buffer_size);
+
+#endif // _VR_KSYNC_KERNEL_H_
