aaf0210e12a20f39d02c6c8a8a6d35e8f88df9f0
Piotr Szuberski <piotr.szuberski@codilime.com>
2019-02-01 16:55:36 +0100

    Refactored ksync
    1. Separated functions operating on KsyncDeviceContext and KsyncResponse
       from vr_ksync.c to ksync_device_context, ksync_response and
       ksync_allocation
    2. Extracted functions that need to be unit-tested from vr_ksync.c
       to separate files: the user and the kernel one.
    3. PairStatusInformation provides the data needed by IRP, allows
       not to use IRP in unit-tested function.
    4. Added kernel memory functions wrappers to win_memory.h
    5. Removed calls to DbgPrint - they were redundant
    6. Removed break in while loop in KsyncParseAndHandleWrite to enable
       bulk msg support.
    
    Partial Jira Bug: WIND-1
    
    Change-Id: Iffd1993f897f77111836c4128b537cf7d4514352

---
+++ windows/ksync/ksync_allocation.h
@@ -0,0 +1,13 @@
+/*
+ * ksync_allocation.h
+ *
+ * Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+ */
+#ifndef __KSYNC_ALLOCATION_H__
+#define __KSYNC_ALLOCATION_H__
+
+#include <windows_types.h>
+
+PVOID KsyncAllocateMemory(size_t bytes);
+
+#endif __KSYNC_ALLOCATION_H__
---
+++ windows/vr_ksync_user.h
@@ -0,0 +1,22 @@
+/*
+ * vr_ksync_user.h -- ksync functions that can be used in user space
+ *
+ * Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+ */
+#ifndef _VR_KSYNC_USER_H_
+#define _VR_KSYNC_USER_H_
+
+#include "ksync_device_context.h"
+
+typedef struct _RESULT_STATUS_INFO RESULT_STATUS_INFO;
+typedef RESULT_STATUS_INFO *PRESULT_STATUS_INFO;
+struct _RESULT_STATUS_INFO {
+    NTSTATUS Status;
+    ULONG Information;
+};
+
+RESULT_STATUS_INFO KsyncParseAndHandleWrite(PKSYNC_DEVICE_CONTEXT ctx,
+                                            PCHAR userBuffer,
+                                            ULONG userBufferSize);
+
+#endif // _VR_KSYNC_USER_H_
