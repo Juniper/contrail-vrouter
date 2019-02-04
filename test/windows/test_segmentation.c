78e915b8676d7d866a9244aea3cca54b79901e95
Michal Clapinski <michal.clapinski@codilime.com>
2019-02-04 17:14:59 +0100

    Fix calculating new packet size in segmentation
    Error was related to the wrong usage of modulus operator in win_packet_splitting.
    1. Fixed error
    2. Added unit test for this case
    
    Closes Jira Bug: WIND-168
    
    Change-Id: Iedf4367fcc054f1b948ec2c4558371fcbe66dd0e

---
+++ test/windows/test_segmentation.c
@@ -0,0 +1,37 @@
+/*
+ * test_segmentation.c
+ *
+ * Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "win_packet_splitting.h"
+
+void
+Test_segmentation_ReturnsCorrectCsum1(void **state)
+{
+    struct SplittingContext pctx;
+    struct vr_ip ip_header;
+
+    pctx.inner_eth_header_size = 20;
+    pctx.inner_headers_size = 60;
+    pctx.maximum_inner_payload_length = 1300;
+    pctx.total_payload_size = 3900;
+
+    fix_packet_length_in_inner_ip_header_of_split_packet(&pctx, &ip_header, true);
+    assert_int_equal(ip_header.ip_len, htons(1340));
+    fix_packet_length_in_inner_ip_header_of_split_packet(&pctx, &ip_header, false);
+    assert_int_equal(ip_header.ip_len, htons(1340));
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(Test_segmentation_ReturnsCorrectCsum1),
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
