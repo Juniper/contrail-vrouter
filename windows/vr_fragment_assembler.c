96c6deee9d4094dbb0c14456f13ef32ae760d865
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2019-02-15 17:06:36 +0100

    Implement fragment assembler properly on Windows
    Implement per-CPU fragment queues.
    For now, the implementation is partially copied from Linux implementation.
    It will be refactored in followup PRs.
    
    Change-Id: Ib448d0515b99c9b64035222a23e0c048348526bd
    Partial-Jira-Bug: WIND-165

--- windows/vr_fragment_assembler.c
+++ windows/vr_fragment_assembler.c
@@ -14,67 +14,83 @@ struct vr_win_fragment_bucket {
 };
 static struct vr_win_fragment_bucket *VrAssemblerTable;
 
+struct vr_win_fragment_queue {
+    KSPIN_LOCK vrwfq_lock;
+    NDIS_HANDLE vrwfq_work;
+    BOOLEAN vrwfq_pending;
+    BOOLEAN vrwfq_initialized;
+    UINT vrwfq_running;
+    struct vr_fragment_queue vrwfq_queue;
+};
+struct vr_win_fragment_queue *vr_wfq_pcpu_queues;
+
 static unsigned int vr_win_assembler_scan_index;
 static int vr_win_assembler_scan_thresh = 1024;
 
-// Adapted from vr_fragment_enqueue.
-// We're only pretending to have per-cpu queue,
-// this function only creates a queue element instead
-// of enqueueing it.
-static struct vr_fragment_queue_element *
-VrCreateFragmentQueueElement(struct vrouter *router,
-        struct vr_packet *pkt, struct vr_forwarding_md *fmd)
-{
-    struct vr_fragment_queue_element *fqe = NULL;
-    struct vr_packet_node *pnode;
+static NDIS_IO_WORKITEM_FUNCTION VrFragmentAssembler;
 
-    fqe = vr_malloc(sizeof(*fqe), VR_FRAGMENT_QUEUE_ELEMENT_OBJECT);
-    if (!fqe) {
-        goto fail;
-    }
-    fqe->fqe_router = router;
-    fqe->fqe_next = NULL;
-
-    pkt->vp_flags &= ~VP_FLAG_FLOW_SET;
-
-    pnode = &fqe->fqe_pnode;
-    vr_flow_fill_pnode(pnode, pkt, fmd);
-
-    return fqe;
-
-fail:
-    if (fqe)
-        vr_free(fqe, VR_FRAGMENT_QUEUE_ELEMENT_OBJECT);
-
-    vr_pfree(pkt, VP_DROP_FRAGMENTS);
-    return NULL;
-}
-
-static void
-VrFragmentAssembler(struct vr_fragment_queue_element* vfqe)
+static VOID
+VrFragmentAssembler(PVOID Context, NDIS_HANDLE NdisIoWorkItemHandle)
 {
     KIRQL old_irql;
-    struct vr_packet_node *pnode;
     uint32_t hash, index;
+
+    struct vr_packet_node *pnode;
     struct vr_win_fragment_bucket *vfb;
+    struct vr_fragment_queue_element *tail, *tail_n, *tail_p, *tail_pn;
+    struct vr_win_fragment_queue *wfq = (struct vr_win_fragment_queue *)Context;
 
-    pnode = &vfqe->fqe_pnode;
-    if (pnode->pl_packet) {
-        hash = vr_fragment_get_hash(pnode);
-        index = (hash % VR_ASSEMBLER_BUCKET_COUNT);
-        vfb = &VrAssemblerTable[index];
+    InterlockedIncrement(&wfq->vrwfq_running);
+    wfq->vrwfq_pending = FALSE;
+    vr_sync_synchronize();
 
-        KeAcquireSpinLock(&vfb->vfb_lock, &old_irql);
-        vr_fragment_assembler(&vfb->vfb_frag_list, vfqe);
-        KeReleaseSpinLock(&vfb->vfb_lock, old_irql);
+    tail = InterlockedExchangePointer(&wfq->vrwfq_queue.vfq_tail, NULL);
+    if (!tail) {
+        InterlockedDecrement(&wfq->vrwfq_running);
+        return;
     }
+
+    /*
+     * first, reverse the list, since packets that came later are at the
+     * head of the list
+     */
+    tail_p = tail->fqe_next;
+    tail->fqe_next = NULL;
+    while (tail_p) {
+        tail_pn = tail_p->fqe_next;
+        tail_p->fqe_next = tail;
+        tail = tail_p;
+        tail_p = tail_pn;
+    }
+
+    /* go through the list and insert it in the assembler work area */
+    while (tail) {
+        tail_n = tail->fqe_next;
+        tail->fqe_next = NULL;
+
+        pnode = &tail->fqe_pnode;
+        if (pnode->pl_packet) {
+            hash = vr_fragment_get_hash(pnode);
+            index = (hash % VR_ASSEMBLER_BUCKET_COUNT);
+            vfb = &VrAssemblerTable[index];
+
+            KeAcquireSpinLock(&vfb->vfb_lock, &old_irql);
+            vr_fragment_assembler(&vfb->vfb_frag_list, tail);
+            KeReleaseSpinLock(&vfb->vfb_lock, old_irql);
+        }
+
+        tail = tail_n;
+    }
+
+    InterlockedDecrement(&wfq->vrwfq_running);
 }
 
 int
 win_enqueue_to_assembler(struct vrouter *router, struct vr_packet *pkt,
         struct vr_forwarding_md *fmd)
 {
-    struct vr_fragment_queue_element *vfqe;
+    KIRQL old_irql;
+    int ret;
     unsigned int cpu;
 
     cpu = vr_get_cpu();
@@ -84,12 +100,18 @@ win_enqueue_to_assembler(struct vrouter *router, struct vr_packet *pkt,
         return -EINVAL;
     }
 
-    // We're going to pretend enqueueing on CPU-list,
-    // by creating a single-element list
-    // and immediately dispatching the work.
-    vfqe = VrCreateFragmentQueueElement(router, pkt, fmd);
-    if (vfqe)
-        VrFragmentAssembler(vfqe);
+    struct vr_win_fragment_queue *fq = &vr_wfq_pcpu_queues[cpu];
+
+    ret = vr_fragment_enqueue(router, &fq->vrwfq_queue, pkt, fmd);
+    if (!ret) {
+        KeAcquireSpinLock(&fq->vrwfq_lock, &old_irql);
+        if (fq->vrwfq_initialized) {
+            if (vr_sync_bool_compare_and_swap_8u(&fq->vrwfq_pending, FALSE, TRUE)) {
+                NdisQueueIoWorkItem(fq->vrwfq_work, VrFragmentAssembler, fq);
+            }
+        }
+        KeReleaseSpinLock(&fq->vrwfq_lock, old_irql);
+    }
 
     return 0;
 }
@@ -118,6 +140,62 @@ VrAssemblerTableScan(void *arg)
 
     vr_win_assembler_scan_index = (i + j) % VR_ASSEMBLER_BUCKET_COUNT;
     return;
+}
+
+static int
+VrFragmentQueueInit(void)
+{
+    unsigned int i, size;
+
+    size = sizeof(struct vr_win_fragment_queue) * vr_num_cpus;
+    vr_wfq_pcpu_queues = vr_zalloc(size, VR_FRAGMENT_QUEUE_OBJECT);
+    if (!vr_wfq_pcpu_queues) {
+        return -ENOMEM;
+    }
+
+    for (i = 0; i < vr_num_cpus; i++) {
+        KeInitializeSpinLock(&vr_wfq_pcpu_queues[i].vrwfq_lock);
+        vr_wfq_pcpu_queues[i].vrwfq_work = NdisAllocateIoWorkItem(VrDriverHandle);
+        if (vr_wfq_pcpu_queues[i].vrwfq_work == NULL) {
+            return -ENOMEM;
+        }
+        vr_wfq_pcpu_queues[i].vrwfq_initialized = TRUE;
+    }
+
+    return 0;
+}
+
+static void
+VrFragmentQueueExit(void)
+{
+    int i;
+
+    if (vr_wfq_pcpu_queues) {
+        for (i = 0; i < vr_num_cpus; i++) {
+            KIRQL old_irql;
+            struct vr_win_fragment_queue *fq = &vr_wfq_pcpu_queues[i];
+
+            if (fq->vrwfq_initialized) {
+                KeAcquireSpinLock(&fq->vrwfq_lock, &old_irql);
+                fq->vrwfq_initialized = FALSE;
+                KeReleaseSpinLock(&fq->vrwfq_lock, old_irql);
+
+                while (fq->vrwfq_pending) {
+                    vr_sync_synchronize();
+                }
+
+                while (fq->vrwfq_running) {
+                    vr_sync_synchronize();
+                }
+
+                NdisFreeIoWorkItem(fq->vrwfq_work);
+            }
+            vr_fragment_queue_free(&fq->vrwfq_queue);
+        }
+
+        vr_free(vr_wfq_pcpu_queues, VR_FRAGMENT_QUEUE_OBJECT);
+        vr_wfq_pcpu_queues = NULL;
+    }
 }
 
 static int
@@ -159,15 +237,20 @@ VrAssemblerTableExit(void)
 int
 VrAssemblerInit(void)
 {
-    // We're not using fragment queue (yet),
-    // only assembler table.
-    return VrAssemblerTableInit();
+    int ret;
+
+    if ((ret = VrFragmentQueueInit()))
+        return ret;
+
+    if ((ret = VrAssemblerTableInit()))
+        return ret;
+
+    return 0;
 }
 
 void
 VrAssemblerExit(void)
 {
-    // We're not using fragment queue (yet),
-    // only assembler table.
+    VrFragmentQueueExit();
     VrAssemblerTableExit();
 }
