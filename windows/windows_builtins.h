4b43b14312302988df56df3992bdaaa5724526b6
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2019-02-18 16:22:06 +0100

    Fix atomic builtin functions on Windows
    Atomic builtin functions should use 'volatile' for all pointers.
    
    Change-Id: I5949dfde57c6adfa720cb79fc6c8507884bb78ff
    Closes-Jira-Bug: WIND-177

--- windows/windows_builtins.h
+++ windows/windows_builtins.h
@@ -18,107 +18,161 @@
 #pragma intrinsic(_BitScanForward)
 
 
-__forceinline UINT16 vr_sync_sub_and_fetch_16u(UINT16 *ptr, UINT16 val) {
-    return _InterlockedExchangeAdd16((PSHORT)ptr, -((SHORT)val)) - val;
+__forceinline UINT16
+vr_sync_sub_and_fetch_16u(volatile UINT16 *ptr, UINT16 val)
+{
+    return _InterlockedExchangeAdd16(ptr, -((SHORT)val)) - val;
 }
 
-__forceinline UINT32 vr_sync_sub_and_fetch_32u(UINT32 *ptr, UINT32 val) {
-    return InterlockedAdd((PLONG)ptr, -((LONG)val));
+__forceinline UINT32
+vr_sync_sub_and_fetch_32u(volatile UINT32 *ptr, UINT32 val)
+{
+    return InterlockedAdd(ptr, -((LONG)val));
 }
 
-__forceinline INT32 vr_sync_sub_and_fetch_32s(INT32 *ptr, INT32 val) {
-    return InterlockedAdd((PLONG)ptr, -((LONG)val));
+__forceinline INT32
+vr_sync_sub_and_fetch_32s(volatile INT32 *ptr, INT32 val)
+{
+    return InterlockedAdd(ptr, -((LONG)val));
 }
 
-__forceinline UINT64 vr_sync_sub_and_fetch_64u(UINT64 *ptr, UINT64 val) {
-    return InterlockedAdd64((PLONGLONG)ptr, -((LONGLONG)val));
+__forceinline UINT64
+vr_sync_sub_and_fetch_64u(volatile UINT64 *ptr, UINT64 val)
+{
+    return InterlockedAdd64(ptr, -((LONGLONG)val));
 }
 
-__forceinline INT64 vr_sync_sub_and_fetch_64s(INT64 *ptr, INT64 val) {
-    return InterlockedAdd64((PLONGLONG)ptr, -((LONGLONG)val));
-}
-
-
-__forceinline UINT16 vr_sync_add_and_fetch_16u(UINT16 *ptr, UINT16 val) {
-    return _InterlockedExchangeAdd16((PSHORT)ptr, (SHORT)val) + val;
-}
-
-__forceinline UINT32 vr_sync_add_and_fetch_32u(UINT32 *ptr, UINT32 val) {
-    return InterlockedAdd((PLONG)ptr, (LONG)val);
-}
-
-__forceinline UINT64 vr_sync_add_and_fetch_64u(UINT64 *ptr, UINT64 val) {
-    return InterlockedExchangeAdd64((PLONGLONG)ptr, (LONGLONG)val) + val;
+__forceinline INT64
+vr_sync_sub_and_fetch_64s(volatile INT64 *ptr, INT64 val)
+{
+    return InterlockedAdd64(ptr, -((LONGLONG)val));
 }
 
 
-__forceinline UINT32 vr_sync_fetch_and_add_32u(UINT32 *ptr, UINT32 val) {
-    return InterlockedExchangeAdd((PLONG)ptr, (LONG)val);
+__forceinline UINT16
+vr_sync_add_and_fetch_16u(volatile UINT16 *ptr, UINT16 val)
+{
+    return _InterlockedExchangeAdd16(ptr, (SHORT)val) + val;
 }
 
-__forceinline UINT64 vr_sync_fetch_and_add_64u(UINT64 *ptr, UINT64 val) {
-    return InterlockedExchangeAdd64((PLONGLONG)ptr, (LONGLONG)val);
+__forceinline UINT32
+vr_sync_add_and_fetch_32u(volatile UINT32 *ptr, UINT32 val)
+{
+    return InterlockedAdd(ptr, (LONG)val);
 }
 
-
-__forceinline UINT16 vr_sync_fetch_and_or_16u(UINT16 *ptr, UINT16 val) {
-    return InterlockedOr16((PSHORT)ptr, (SHORT)val);
-}
-
-
-__forceinline UINT16 vr_sync_and_and_fetch_16u(UINT16 *ptr, UINT16 val) {
-    return InterlockedAnd16((PSHORT)ptr, (SHORT)val) & ((SHORT)val);
-}
-
-__forceinline UINT32 vr_sync_and_and_fetch_32u(UINT32 *ptr, UINT32 val) {
-    return InterlockedAnd((PLONG)ptr, (LONG)val) & ((LONG)val);
+__forceinline UINT64
+vr_sync_add_and_fetch_64u(volatile UINT64 *ptr, UINT64 val)
+{
+    return InterlockedExchangeAdd64(ptr, (LONGLONG)val) + val;
 }
 
 
-__forceinline bool vr_sync_bool_compare_and_swap_8s(UINT8 *ptr, UINT8 oldval, UINT8 newval) {
-    return _InterlockedCompareExchange8((PCHAR)ptr, newval, oldval) == oldval;
+__forceinline UINT32
+vr_sync_fetch_and_add_32u(volatile UINT32 *ptr, UINT32 val)
+{
+    return InterlockedExchangeAdd(ptr, (LONG)val);
+}
+
+__forceinline UINT64
+vr_sync_fetch_and_add_64u(volatile UINT64 *ptr, UINT64 val)
+{
+    return InterlockedExchangeAdd64(ptr, (LONGLONG)val);
 }
 
 
-__forceinline bool vr_sync_bool_compare_and_swap_8u(UINT8 *ptr, UINT8 oldval, UINT8 newval) {
-    return _InterlockedCompareExchange8((PCHAR)ptr, (CHAR)newval, (CHAR)oldval) == (CHAR)oldval;
+__forceinline UINT16
+vr_sync_fetch_and_or_16u(volatile UINT16 *ptr, UINT16 val)
+{
+    return InterlockedOr16(ptr, (SHORT)val);
 }
 
-__forceinline bool vr_sync_bool_compare_and_swap_16u(UINT16 *ptr, UINT16 oldval, UINT16 newval) {
-    return _InterlockedCompareExchange16((PSHORT)ptr, (SHORT)newval, (SHORT)oldval) == (SHORT)oldval;
+
+__forceinline UINT16
+vr_sync_and_and_fetch_16u(volatile UINT16 *ptr, UINT16 val)
+{
+    return InterlockedAnd16(ptr, (SHORT)val) & ((SHORT)val);
 }
 
-__forceinline bool vr_sync_bool_compare_and_swap_32u(UINT32 *ptr, UINT32 oldval, UINT32 newval) {
-    return _InterlockedCompareExchange((PLONG)ptr, (LONG)newval, (LONG)oldval) == (LONG)oldval;
+__forceinline UINT32
+vr_sync_and_and_fetch_32u(volatile UINT32 *ptr, UINT32 val)
+{
+    return InterlockedAnd(ptr, (LONG)val) & ((LONG)val);
 }
 
-__forceinline bool vr_sync_bool_compare_and_swap_p(void **ptr, void *oldval, void *newval) {
+
+__forceinline bool
+vr_sync_bool_compare_and_swap_8s(
+    volatile UINT8 *ptr, UINT8 oldval, UINT8 newval)
+{
+    return _InterlockedCompareExchange8(ptr, newval, oldval) == oldval;
+}
+
+__forceinline bool
+vr_sync_bool_compare_and_swap_8u(
+    volatile UINT8 *ptr, UINT8 oldval, UINT8 newval)
+{
+    CHAR result = _InterlockedCompareExchange8(ptr, (CHAR)newval, (CHAR)oldval);
+    return result == (CHAR)oldval;
+}
+
+__forceinline bool
+vr_sync_bool_compare_and_swap_16u(
+    volatile UINT16 *ptr, UINT16 oldval, UINT16 newval)
+{
+    SHORT result = _InterlockedCompareExchange16(
+        ptr, (SHORT)newval, (SHORT)oldval);
+    return result == (SHORT)oldval;
+}
+
+__forceinline bool
+vr_sync_bool_compare_and_swap_32u(
+    volatile UINT32 *ptr, UINT32 oldval, UINT32 newval)
+{
+    LONG result = _InterlockedCompareExchange(ptr, (LONG)newval, (LONG)oldval);
+    return result == (LONG)oldval;
+}
+
+__forceinline bool
+vr_sync_bool_compare_and_swap_p(
+    void * volatile *ptr, void *oldval, void *newval)
+{
     return _InterlockedCompareExchangePointer(ptr, newval, oldval) == oldval;
 }
 
 
-__forceinline UINT16 vr_sync_val_compare_and_swap_16u(UINT16 *ptr, UINT16 oldval, UINT16 newval) {
-    return _InterlockedCompareExchange16((PSHORT)ptr, newval, oldval);
+__forceinline UINT16
+vr_sync_val_compare_and_swap_16u(
+    volatile UINT16 *ptr, UINT16 oldval, UINT16 newval)
+{
+    return _InterlockedCompareExchange16(ptr, newval, oldval);
 }
 
-__forceinline UINT8 vr_sync_lock_test_and_set_8u(UINT8 *ptr, UINT8 val) {
-    return InterlockedExchange8((PCHAR)ptr, val);
+__forceinline UINT8
+vr_sync_lock_test_and_set_8u(volatile UINT8 *ptr, UINT8 val)
+{
+    return InterlockedExchange8(ptr, val);
 }
 
 
-__forceinline void vr_sync_synchronize() {
+__forceinline void
+vr_sync_synchronize()
+{
     _ReadWriteBarrier();    // compiler memory barrier (compiler level fence)
     MemoryBarrier();        // cpu memory barrier (hardware level fence)
 }
 
 
-__forceinline int vr_ffs_32(int x) {
+__forceinline int
+vr_ffs_32(int x)
+{
     ULONG index, mask = (ULONG)x;
     UCHAR isNonzero = _BitScanForward(&index, mask);
-    if (isNonzero)
+    if (isNonzero) {
         return index + 1;
-    else
+    } else {
         return 0;
+    }
 }
 
 #endif /* __WINDOWS_BUILTINS_H__ */
