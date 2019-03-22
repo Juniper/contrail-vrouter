9012da17ac993b37fc9a92bf2b30573e22079b56
Haji Mohamed <hajim@juniper.net>
2019-03-22 04:31:28 -0700

    vrouter debug enhancement: Clear dropstats counter for all cores during runtime. command: dropstats --clear Closes-Jira-Bug: CEM-3323
    Change-Id: I91283226bcfb88d13c2f6bd3fadc13e1ab75fee6

--- utils/dropstats.c
+++ utils/dropstats.c
@@ -29,10 +29,13 @@
 #include "vr_packet.h"
 
 static struct nl_client *cl;
-static int help_set, core_set, offload_set, log_set;
+static int help_set, core_set, offload_set, log_set, clear_set;
 static unsigned int core = (unsigned)-1;
 static unsigned int stats_index = 0;
 static int vr_get_pkt_drop_log(struct nl_client *cl,int core,int stats_index);
+void vr_print_pkt_drop_log(vr_pkt_drop_log_req *pkt_log);
+int vr_pkt_drop_log_request(struct nl_client *cl, unsigned int router_id,
+         unsigned int core, int log_idx );
 
 static void pkt_drop_log_req_process(void *s_req) {
 
@@ -155,6 +158,13 @@ drop_stats_req_process(void *s_req)
 {
     vr_drop_stats_req *stats = (vr_drop_stats_req *)s_req;
     int platform = get_platform();
+
+    if(stats->h_op == SANDESH_OP_RESET)
+    {
+        printf("\nDropstats counters cleared successfully on all cores \n\n");
+        return;
+    }
+
     if (core == (unsigned)-2)
         printf("Statistics for NIC offloads\n\n");
     else if (core != (unsigned)-1)
@@ -281,11 +291,18 @@ vr_get_drop_stats(struct nl_client *cl)
     return 0;
 }
 
+static int
+vr_clear_drop_stats(struct nl_client *cl)
+{
+    vr_drop_stats_reset(cl);
+}
+
 enum opt_index {
     HELP_OPT_INDEX,
     CORE_OPT_INDEX,
     OFFL_OPT_INDEX,
     LOG_OPT_INDEX,
+    CLEAR_OPT_INDEX,
     MAX_OPT_INDEX,
 };
 
@@ -294,6 +311,7 @@ static struct option long_options[] = {
     [CORE_OPT_INDEX]    =   {"core",    required_argument,  &core_set,      1},
     [OFFL_OPT_INDEX]    =   {"offload", no_argument,        &offload_set,   1},
     [LOG_OPT_INDEX]     =   {"log",     required_argument,  &log_set,       1},
+    [CLEAR_OPT_INDEX]   =   {"clear",   no_argument,        &clear_set,     1},
     [MAX_OPT_INDEX]     =   {"NULL",    0,                  0,              0},
 };
 
@@ -405,7 +423,16 @@ main(int argc, char *argv[])
     if (!cl)
         return -1;
 
-    vr_get_drop_stats(cl);
+    if (option_index == CLEAR_OPT_INDEX)
+    {
+        vr_clear_drop_stats(cl);
+
+        ret = vr_recvmsg(cl, false);
+        if (ret <= 0)
+            return ret;
+    }
+    else
+        vr_get_drop_stats(cl);
 
     return 0;
 }
