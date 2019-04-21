0bd94d48143e572b53829c790971779e11e64615
Kiran Kasim <kirankn@juniper.net>
2019-04-21 10:01:23 -0700

    Fix for Invalid label programming in VXLAN routing
    In case of VXLAN routing, for vxlan id's whose top 4 bits are non-zero,
       there was a truncation happening in mtrie. The reason was that the
       label was assumed to be MPLS label which is 20bits. But in case of
       VXLAN routing, mtrie lookup happends for VXLAN labels which are 24
       bits. Fixing the mask to 24 bits.
    
     Testing
     The following commands are executed to program a VXLAN id (11158251)
        with the top 4 bits are non-zero
    
     nh --create 350 --vrf 2 --type 3 --oif 0 --smac 00:25:90:a5:39:af
     --dmac 00:25:90:a5:3a:fd --sip 8.0.0.2 --dip 8.0.0.3 --vxlan --sport
     10000 --dport 10000 --l3_vxlan 00:25:90:a5:3a:fd
    
     rt -c -n 350 -p 10.1.1.6 -l 32 -t 11158251 -f 0 -v 2 -x 0x1
    
     Before the fix, the label in incorrectly programmed -
    
     (vrouter-agent-dpdk)[root@a7s4-kiran /]$ rt --dump 2 --family inet
     |grep 10.1.1.6
     10.1.1.6/32            32            L      672491            350
     -
    
     After the fix, the label is properly programmed -
    
     (vrouter-agent-dpdk)[root@a7s4-kiran /]$ rt --dump 2 |  grep
     "10\.1\.1\.6\>"
     10.1.1.6/32            32            L      11158251            350
     -
    
     Closes-Jira-Bug: CEM-4968
    
    Change-Id: I033b07911c8d371b8447637c58d3cfc0eafaa1f0

--- dp-core/vr_ip_mtrie.c
+++ dp-core/vr_ip_mtrie.c
@@ -800,9 +800,9 @@ mtrie_delete(struct vr_rtable * _unused, struct vr_route_req *rt)
     }
 
     if (!(rt->rtr_req.rtr_label_flags & VR_RT_LABEL_VALID_FLAG)) {
-        rt->rtr_req.rtr_label = 0xFFFFF;
+        rt->rtr_req.rtr_label = 0xFFFFFF;
     } else {
-        rt->rtr_req.rtr_label &= 0xFFFFF;
+        rt->rtr_req.rtr_label &= 0xFFFFFF;
     }
 
     __mtrie_delete(rt, &rtable->root, 0, 0, 1);
@@ -1103,9 +1103,9 @@ mtrie_add(struct vr_rtable * _unused, struct vr_route_req *rt)
     }
 
     if (!(rt->rtr_req.rtr_label_flags & VR_RT_LABEL_VALID_FLAG)) {
-        rt->rtr_req.rtr_label = 0xFFFFF;
+        rt->rtr_req.rtr_label = 0xFFFFFF;
     } else {
-        rt->rtr_req.rtr_label &= 0xFFFFF;
+        rt->rtr_req.rtr_label &= 0xFFFFFF;
     }
 
     ret = __mtrie_add(mtrie, rt, 1);
@@ -1164,7 +1164,7 @@ mtrie_alloc_vrf(unsigned int vrf_id, unsigned int family)
         mtrie->root.entry_type = ENTRY_TYPE_NEXTHOP;
         mtrie_table = vn_rtable[index];
         mtrie_table[vrf_id] = mtrie;
-        mtrie->root.entry_label = 0xFFFFF;
+        mtrie->root.entry_label = 0xFFFFFF;
         mtrie->root.entry_label_flags = 0;
     }
 
@@ -1389,7 +1389,7 @@ vdata_mtrie_init (unsigned int prefix_len, void *data)
         mtrie->root.entry_prefix_len = prefix_len;
         mtrie->root.entry_vdata_p = data;
         mtrie->root.entry_bridge_index =  VR_BE_INVALID_INDEX;
-        mtrie->root.entry_label = 0xFFFFF;
+        mtrie->root.entry_label = 0xFFFFFF;
     }
 
     return mtrie;
