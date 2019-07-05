e56081d15df3d871ff1d82b3049d1d2aadc096de
Haji Mohamed <hajim@juniper.net>
2019-07-05 02:33:16 -0700

    Testcase for CEM-4574 and merge code to master branch Closes-Jira-Bug: CEM-6947
    Change-Id: Ie19c2462f4d38d27f727ea6f519dd177b6d20fc5

--- utils/vtest/include/vtest.h
+++ utils/vtest/include/vtest.h
@@ -23,6 +23,42 @@
 #define VT_MESSAGES_MAX             (1024 * 512)
 #define VT_MAX_FLOW                 500000
 
+
+#if __BYTE_ORDER == __BIG_ENDIAN
+#  define ntohll(n) (n)
+#  define htonll(n) (n)
+# if defined(__GNUC__) && defined(__GLIBC__)
+#  include <byteswap.h>
+#  define htolell(n) bswap_64(n)
+#  define letohll(n) bswap_64(n)
+# else /* GNUC & GLIBC */
+#  define bswap_64(n) \
+      ( (((n) & 0xff00000000000000ull) >> 56) \
+      | (((n) & 0x00ff000000000000ull) >> 40) \
+      | (((n) & 0x0000ff0000000000ull) >> 24) \
+      | (((n) & 0x000000ff00000000ull) >> 8)  \
+      | (((n) & 0x00000000ff000000ull) << 8)  \
+      | (((n) & 0x0000000000ff0000ull) << 24) \
+      | (((n) & 0x000000000000ff00ull) << 40) \
+      | (((n) & 0x00000000000000ffull) << 56) )
+#  define htolell(n) bswap_64(n)
+#  define letohll(n) bswap_64(n)
+# endif /* GNUC & GLIBC */
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
+#  define htolell(n) (n)
+#  define letohll(n) (n)
+# if defined(__GNUC__) && defined(__GLIBC__)
+#  include <byteswap.h>
+#  define ntohll(n) bswap_64(n)
+#  define htonll(n) bswap_64(n)
+# else /* GNUC & GLIBC */
+#  define ntohll(n) ( (((uint64_t)ntohl(n)) << 32) + ntohl(n >> 32) )
+#  define htonll(n) ( (((uint64_t)htonl(n)) << 32) + htonl(n >> 32) )
+# endif /* GNUC & GLIBC */
+#else /* __BYTE_ORDER */
+# error "Can't define htonll or ntohll!"
+#endif
+
 struct packet_interface {
    unsigned short vif_id;
    //Todo send a message for map interface id to name interface
