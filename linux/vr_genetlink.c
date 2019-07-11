f7cf5ed1b3f3c0c8bf25e80f493614e105e0cf1f
Antoine Eiche <antoine.eiche@cloudwatt.com>
2019-07-11 17:07:44 +0200

    netlink: fix memory leak on message broadcasting
    When the message is broadcasted, the socket buffer is not freed. Thx
    to the kernel leak detector, we get this message:
    
         unreferenced object 0xffff98932a522100 (size 256):
           comm "vif", pid 15785, jiffies 4327283873 (age 9138.142s)
           hex dump (first 32 bytes):
             00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
             00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
           backtrace:
             [<ffffffff8a579f6a>] kmemleak_alloc+0x4a/0xa0
             [<ffffffff8a1d6d26>] kmem_cache_alloc_node+0x136/0x1e0
             [<ffffffff8a4a8aec>] __alloc_skb+0x4c/0x1c0
             [<ffffffffc063e011>] netlink_trans_alloc+0x21/0x60 [vrouter]
             [<ffffffffc063f005>] vr_message_multi_response+0xd5/0x140 [vrouter]
             [<ffffffffc064b490>] vr_interface_get+0x140/0x1d0 [vrouter]
             [<ffffffffc064c7cd>] vr_interface_req_process+0x11d/0x290 [vrouter]
             [<ffffffffc0635bdf>] sandesh_decode+0xef/0x210 [vrouter]
             [<ffffffffc063f354>] sandesh_proto_encode+0x24/0x60 [vrouter]
             [<ffffffffc063ebc3>] vr_message_request+0x43/0x60 [vrouter]
             [<ffffffffc063e0a7>] netlink_trans_request+0x57/0x250 [vrouter]
             [<ffffffff8a4f568a>] genl_family_rcv_msg+0x1ba/0x370
             [<ffffffff8a4f58ca>] genl_rcv_msg+0x8a/0xd0
             [<ffffffff8a4f4d54>] netlink_rcv_skb+0x54/0xe0
             [<ffffffff8a4f54b8>] genl_rcv+0x28/0x40
             [<ffffffff8a4f4786>] netlink_unicast+0x176/0x220
    
    It seems this socket buffer is released by the `netlink_unicast`
    function but not by `genlmsg_multicast`. In the latter case, we then
    have to free it explicitly.
    
    Change-Id: I00a0d5fe9293d8bc216ab02b88fa7e96744d9332
    closes-jira-bug: TFB-1456

--- linux/vr_genetlink.c
+++ linux/vr_genetlink.c
@@ -141,6 +141,7 @@ netlink_trans_request(struct sk_buff *in_skb, struct genl_info *info)
             buf = response->vr_message_buf;
             skb = netlink_skb(buf);
             if (!skb)
+                response->vr_message_buf = NULL;
                 goto next;
 
             len = response->vr_message_len;
@@ -156,6 +157,7 @@ netlink_trans_request(struct sk_buff *in_skb, struct genl_info *info)
             nla->nla_type = NL_ATTR_VR_MESSAGE_PROTOCOL;
 
             netlink_unicast(in_skb->sk, skb, netlink_id, MSG_DONTWAIT);
+            response->vr_message_buf = NULL;
         } else {
             // If there is no listener, we don't broadcast
             if (!netlink_has_listeners(in_skb->sk, vrouter_genl_family.mcgrp_offset)) {
@@ -179,7 +181,6 @@ netlink_trans_request(struct sk_buff *in_skb, struct genl_info *info)
             genlmsg_multicast(&vrouter_genl_family, skb, 0, 0, GFP_KERNEL);
         }
 next:
-        response->vr_message_buf = NULL;
         vr_message_free(response);
     }
 
