f5e8f952131e1ab6706b93b7651cb6f8b68972ae
hajim <hajim@juniper.net>
2019-08-08 16:56:06 +0530

    CEM-5596 - Monitoring of DPDK Bond members
    Closes-jira-bug:CEM-5596
    
    - Callback notifiacation for Master/Slave bond interface members has
       been
       implemented.
        - Notifying vrouter-agent about the state change
         - Display Bond interface member status in "vif --list" output.
    Updated patchset:
     - Included vlan info, Fwdcore to display as part of physical interface
     - Display NH id for all interfaces.
     - Sending vif interface id as part of netlink message to agent
         Blueprint:
         https://review.opencontrail.org/c/Juniper/contrail-specs/+/52710
    Updated patchset:
     - Review comments updated.
    
    Depends-on: I03d809a8e8f0dfd5c23c987d9536be116c2417bc
    
    Change-Id: I5985c6c4b00d17a4320a860b66640d0990409d9c

--- dpdk/vr_dpdk_ethdev.c
+++ dpdk/vr_dpdk_ethdev.c
@@ -15,6 +15,7 @@
  */
 
 #include "vr_dpdk.h"
+#include "vr_dpdk_usocket.h"
 
 #include <vr_mpls.h>
 
@@ -28,6 +29,8 @@
 #include <rte_ip.h>
 #include <rte_port_ethdev.h>
 #include <rte_udp.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
 
 extern int vr_rxd_sz, vr_txd_sz;
 extern unsigned int datapath_offloads;
@@ -93,6 +96,9 @@ struct rte_eth_conf ethdev_conf = {
         .drop_queue = 0,
         .flex_conf = {
         },
+    },
+    .intr_conf = {
+        .lsc = 1, /* Enable Link status interrupts */
     },
 };
 
@@ -760,6 +766,199 @@ vr_dpdk_ethdev_bond_port_match(uint8_t port_id, struct vr_dpdk_ethdev *ethdev)
     return false;
 }
 
+
+/*
+ * Function to send bond interface state to Agent
+ * info->status 0 - down
+ * info->status 1 - up
+ * Called when bond master/slave interface goes down or comes up
+ */
+static void
+vr_dpdk_nl_send_bond_intf_state(struct vr_dpdk_bond_member_info *info,
+        unsigned type, uint8_t vif_idx)
+{
+    int nl_fd;
+    char buf[1024];
+    struct nlmsghdr *nlh;
+    struct ifinfomsg *ifinfo;
+    struct nlattr *nla;
+    int len, n;
+    char *if_name_buf;
+    struct sockaddr_nl sa;
+
+    if (info == NULL) {
+        RTE_LOG(ERR, VROUTER, "Bond member info is NULL\n");
+        return;
+    }
+    memset(buf, 0, sizeof(buf));
+    memset(&sa, 0, sizeof(sa));
+    sa.nl_family = AF_NETLINK;
+    sa.nl_pid = 0;
+    sa.nl_groups = RTMGRP_LINK | RTMGRP_IPV4_IFADDR;
+
+    nl_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+    if (nl_fd < 0) {
+        RTE_LOG(ERR, VROUTER, "Error creating netlink socket\n");
+        goto error;
+    }
+    bind(nl_fd, (struct sockaddr *)&sa, sizeof(sa));
+    nlh = (struct nlmsghdr *)buf;
+
+    nlh->nlmsg_len = NLMSG_HDRLEN + sizeof(struct ifinfomsg);
+    nlh->nlmsg_type = RTM_NEWLINK;
+    nlh->nlmsg_flags = NLM_F_REQUEST;
+    nlh->nlmsg_seq = 1;
+    nlh->nlmsg_pid = 0;
+    ifinfo = (struct ifinfomsg *)(buf + NLMSG_HDRLEN);
+
+    ifinfo->ifi_family = 0;
+    ifinfo->ifi_type = type;
+
+    /* Set index to -1 as agent needs only the interface name */
+    ifinfo->ifi_index = -1;
+
+    if (info->status)
+        ifinfo->ifi_flags = IFF_MULTICAST|IFF_RUNNING|IFF_BROADCAST|IFF_UP;
+    else
+        ifinfo->ifi_flags = IFF_BROADCAST|IFF_UP;
+    /* ifi_change needs to be set to 0xFFFFFFFF by default
+     * as its a reserved field
+     */
+    ifinfo->ifi_change = 0xFFFFFFFF;
+
+    nla = (struct nlattr *)(buf + NLMSG_HDRLEN + sizeof(struct ifinfomsg));
+
+    if_name_buf = (char *)nla + NLA_HDRLEN;
+
+    sprintf(if_name_buf,"%s %s %x", info->intf_name, info->intf_drv_name,
+            vif_idx);
+
+    len = NLA_HDRLEN + NLA_ALIGN(strlen(if_name_buf) + 1);
+
+    nla->nla_len = len;
+    nla->nla_type = IFLA_IFNAME;
+
+
+    len += (NLMSG_HDRLEN + sizeof(struct ifinfomsg));
+
+    nlh->nlmsg_len = len;
+
+    n = sendto(nl_fd, buf, len, 0, (struct sockaddr *)&sa, sizeof(sa));
+    if (n != len) {
+        RTE_LOG(ERR, VROUTER, "Error sending netlink bond interface message\n");
+    }
+    else
+        RTE_LOG(INFO, VROUTER, "Notifed Link status update to agent for \
+                interface name %s\n",
+            info->intf_name);
+
+    close(nl_fd);
+error:
+    return;
+}
+
+static int
+vr_dpdk_bond_intf_callback(uint16_t port_id, enum rte_eth_event_type type,
+        void *param, void *ret_param __rte_unused)
+{
+    int ret;
+    char *str[] = {"UP", "DOWN"};
+    struct vr_dpdk_bond_member_info member_info;
+    struct rte_eth_link link;
+    unsigned bond_type;
+    uint32_t dev_flags;
+    /* TODO: Currently, we have one physical interface and vif_idx always 0
+     * For multi physical interface, need to send corresponding master vif_idx
+     * for slave interfaces */
+    uint8_t vif_idx = 0;
+
+    memset(&member_info, 0, sizeof(member_info));
+
+    rte_eth_link_get_nowait(port_id, &link);
+    member_info.status = link.link_status;
+
+    ret = rte_eth_dev_get_name_by_port(port_id, member_info.intf_name);
+    if(ret != 0)
+        RTE_LOG(ERR, VROUTER, "Error getting bond interface name\n");
+
+    if(rte_eth_devices[port_id].device->driver->name != NULL)
+        strcpy(member_info.intf_drv_name,
+                rte_eth_devices[port_id].device->driver->name);
+
+    RTE_LOG(INFO, VROUTER, "Port ID: %d Link Status: %s intf_name:%s \
+            drv_name:%s \n\n", port_id, (link.link_status?str[0]:str[1]),
+            member_info.intf_name, member_info.intf_drv_name);
+
+    dev_flags = rte_eth_devices[port_id].data->dev_flags;
+    bond_type = (dev_flags & RTE_ETH_DEV_BONDED_SLAVE) ? VR_DPDK_BOND_SLAVE :
+        VR_DPDK_MASTER;
+
+    vr_dpdk_nl_send_bond_intf_state(&member_info, bond_type, vif_idx);
+
+    return 0;
+}
+
+static void
+vr_dpdk_notify_bond_init(uint8_t port, uint8_t vif_idx)
+{
+    int ret = 0, dev_flags;
+    uint8_t bond_type;
+
+    struct vr_dpdk_bond_member_info member_info;
+
+    memset(&member_info, 0, sizeof(member_info));
+
+    ret = rte_eth_dev_get_name_by_port(port, member_info.intf_name);
+    if(ret != 0)
+        RTE_LOG(ERR, VROUTER, "Error getting bond interface name\n");
+
+    if(rte_eth_devices[port].device->driver->name != NULL)
+        strcpy(member_info.intf_drv_name,
+                rte_eth_devices[port].device->driver->name);
+
+    dev_flags = rte_eth_devices[port].data->dev_flags;
+    bond_type = (dev_flags & RTE_ETH_DEV_BONDED_SLAVE) ? VR_DPDK_BOND_SLAVE :
+        VR_DPDK_MASTER;
+
+    /* Notifying agent about the Fabric(Master)/Slave interface members
+     * Default link status is DOWN */
+    vr_dpdk_nl_send_bond_intf_state(&member_info, bond_type, vif_idx);
+}
+
+static void
+vr_dpdk_bond_intf_cb_register(struct vr_dpdk_ethdev *ethdev)
+{
+    int i = 0, ret = 0;
+    /* Fetching port-id for master bond interface */
+    uint8_t port_id = ethdev->ethdev_port_id;
+
+
+    /* Registering callback notification for Master bond interface */
+    ret = rte_eth_dev_callback_register(port_id, RTE_ETH_EVENT_INTR_LSC,
+            vr_dpdk_bond_intf_callback, NULL);
+
+    if(ret)
+        RTE_LOG(ERR, VROUTER, "Failed to setup callback for event \
+                RTE_ETH_EVENT_INTR_LSC for portid: %d\n", port_id);
+    else
+        vr_dpdk_notify_bond_init(port_id, ethdev->ethdev_vif_idx);
+
+    /* For slave interface*/
+    for (i = 0; i < ethdev->ethdev_nb_slaves; i++) {
+
+        port_id = ethdev->ethdev_slaves[i];
+
+        /* Registering callback notification for slave bond interfaces */
+        ret = rte_eth_dev_callback_register(port_id, RTE_ETH_EVENT_INTR_LSC,
+                vr_dpdk_bond_intf_callback, NULL);
+        if(ret)
+            RTE_LOG(ERR, VROUTER, "Failed to setup callback for event \
+                    RTE_ETH_EVENT_INTR_LSC for portid: %d\n", port_id);
+        else
+            vr_dpdk_notify_bond_init(port_id, ethdev->ethdev_vif_idx);
+    }
+}
+
 /* Init ethernet device */
 int
 vr_dpdk_ethdev_init(struct vr_dpdk_ethdev *ethdev, struct rte_eth_conf *dev_conf)
@@ -789,6 +988,8 @@ vr_dpdk_ethdev_init(struct vr_dpdk_ethdev *ethdev, struct rte_eth_conf *dev_conf
 #endif
         dpdk_ethdev_bond_info_update(ethdev);
     }
+
+    vr_dpdk_bond_intf_cb_register(ethdev);
   
     ret = dpdk_ethdev_queues_setup(ethdev);
     if (ret < 0)
