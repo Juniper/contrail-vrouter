1bab85471a0b37e0be7c6aa0bee456596ea86845
hajim <hajim@juniper.net>
2019-10-24 14:39:24 +0530

    Dummy Review Closes-jira-bug: CEM-9436 Depends-On: Id79eb261efec2b9ace54989d336dd8cbbf1ee6b9
    Change-Id: I27d7607f8c5f0fb12b748a51dadb23836d2636ca

--- utils/vtest_py/setup_venv.sh
+++ utils/vtest_py/setup_venv.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+set -x
 
 venv_dir=$1
 req_file_path=$2
 
+echo "venv_dir= $venv_dir"
+echo "req_file_path= $req_file_path"
+
 virtualenv $venv_dir
 source  $venv_dir/bin/activate
+echo "in venv"
 pip install -r $req_file_path
+echo "executing pip install"
+echo "venv_dir= $venv_dir"
+echo "req_file_path= $req_file_path"
 deactivate
 
