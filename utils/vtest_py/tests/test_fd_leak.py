d20bc25b0ec99622ea8d2698e4890228f7508492
Kiran <kirankn@juniper.net>
2019-12-14 07:16:30 +0000

    Revert fix for CEM-9116
    The fix is exposing some more issues relating to vrouter agent.
    Reverting it for now.
    
    Closes-Jira-Bug: CEM-11181
    Change-Id: I6f08bab45753ca12ca31aaaff9317c4c88eb2984

--- utils/vtest_py/tests/test_fd_leak.py
+++ utils/vtest_py/tests/test_fd_leak.py
@@ -10,6 +10,7 @@ import vtconst
 import time
 from vtest_lib import *
 
+@pytest.mark.skip(reason="failing because of vr_uvh_cl_timer_setup() not setup")
 def test_fd_leak(vrouter_test_fixture):
 
     vt = vtest("test_fd_leak")
