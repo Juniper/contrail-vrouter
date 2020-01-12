14101f0d876755001c2b31ce144f6c95a50e3369
Manish Sahu <msahu@juniper.net>
2020-01-12 23:12:50 +0530

    vtest_py infra improvements
    1. Convert run_test.sh to python script
    2. Move tests directory to $SB/vrouter/tests
    3. Move vtest_py vtest specific libraries to $SB/vrouter/utils/pylib
    4. Implement business logic for classes mentioned in skeletal
       implementation CEM-11303
    5. Move UTs' test_basic, test_basic1, test_basic2, test_vrf_table
       to new infra
    
    Closes-Jira-Bug: CEM-8197
    Change-Id: Ia369f2c46e0e040b6c5f7f9670a88608fd915f60

---
+++ tests/run_test.py
@@ -0,0 +1,204 @@
+#!/bin/python
+
+import os
+from os import path
+import sys
+import argparse
+import platform
+import subprocess
+import logging
+
+logging.basicConfig(filename='vtest_py.log',
+                    filemode='w',
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(message)s')
+
+
+def parse(cmd):
+
+    if (len(cmd) == 1):
+        print("Use -h or --help option for usage detail")
+        return
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-vr', '--vrouter', required=False,
+                        help="Specify vrouter path")
+    parser.add_argument('-vt', '--vtest', required=False,
+                        help="specify vtest path")
+    parser.add_argument('-sp', '--socket', required=False,
+                        help="specify socket path")
+    parser.add_argument('-v', '--venv', required=False,
+                        help="specify vtest_py venv path")
+    parser.add_argument('-vt_only', '--vtest_only',
+                        help="run vTest alone", action="store_true")
+    parser.add_argument('-vr_only', '--vrouter_only',
+                        help="run vRouter alone", action="store_true")
+    parser.add_argument('-t', '--test', required=False,
+                        help="test a specific file")
+    parser.add_argument('-gxml', '--xml', required=False,
+                        help="tpecify xml file")
+    parser.add_argument("-a", "--all",
+                        help="run all tests", action="store_true")
+    parser.add_argument("-p", "--pycodestyle",
+                        help="run pycodestyle check", action="store_true")
+    parser.add_argument("-f", "--flake",
+                        help="run flake check", action="store_true")
+    parser.add_argument("-c", "--cli", required=False,
+                        help="run vrouter commands like 'vif --list', etc")
+    
+    vrouter_path = os.environ.get('VROUTER_DPDK_PATH')
+    vtest_path = os.environ.get('VTEST_PATH')
+    socket_path = os.environ.get('VROUTER_SOCKET_PATH')
+    vtest_py_venv_path = os.environ.get('VTEST_PY_VENV_PATH')
+    sb_path = os.environ.get('PWD') + '/../../build'
+
+    args = vars(parser.parse_args())
+    test_opt = args['test']
+
+    if args['vrouter'] is None:
+        if vrouter_path is None:
+            path_cmd = 'realpath {}/debug/vrouter/dpdk/contrail-vrouter-dpdk'.\
+                        format(sb_path)
+            vrouter_path = subprocess.check_output(path_cmd, shell=True)[:-1]
+        logging.info("Using default vrouter path - {}".format(vrouter_path))
+    else:
+        vrouter_path = args['vrouter']
+    if not path.exists(vrouter_path):
+        logging.error("vRouter path not set")
+        return
+    os.environ['VROUTER_DPDK_PATH'] = vrouter_path
+
+    if args['vtest'] is None:
+        if vtest_path is None:
+            path_cmd = 'realpath {}/debug/vrouter/utils/vtest/vtest'.format(
+                        sb_path)
+            vtest_path = subprocess.check_output(path_cmd, shell=True)[:-1]
+        logging.info("Using default vtest path - {}".format(vtest_path))
+    else:
+        vtest_path = args['vtest']
+    if not path.exists(vtest_path):
+        logging.error("vtest path not set")
+        return
+    os.environ['VTEST_PATH'] = vtest_path
+
+    if args['socket'] is None:
+        if socket_path is None: 
+            path_cmd = \
+                'realpath {}/debug/vrouter/utils/vtest_py_venv/var/run/vrouter'.\
+                format(sb_path)
+            socket_path = subprocess.check_output(path_cmd, shell=True)[:-1]
+        logging.info("Using default socket path - {}".format(vtest_path))
+    else:
+        socket_path = args['socket']
+    if not path.exists(socket_path):
+        logging.error("socket path not set")
+        return
+    os.environ['VROUTER_SOCKET_PATH'] = socket_path
+
+    if args['venv'] is None:
+        if vtest_py_venv_path is None:
+            path_cmd = 'realpath {}/debug/vrouter/utils/vtest_py_venv'.format(
+                sb_path)
+            vtest_py_venv_path = \
+                    subprocess.check_output(path_cmd, shell=True)[:-1]
+        logging.info("Using default venv path - {}".format(args['venv']))
+    else:
+        vtest_py_venv_path = args['venv']
+    if not path.exists(vtest_py_venv_path):
+        logging.error("venv path not set")
+        return
+    os.environ['VTEST_PY_VENV_PATH'] = vtest_py_venv_path 
+
+    vif_utilily_path = sb_path + '/debug/vrouter/utils/'
+    if(args['cli'] == 'vif --list'):
+        cmd = '{}{} --sock-dir {}'.format(vif_utilily_path, args['cli'],
+                                          args['socket'])
+        os.system(cmd)
+        return
+
+    logging.info("\nRunning tests with following params:")
+    logging.info("VROUTER_DPDK_PATH: {}".format(vrouter_path))
+    logging.info("VTEST_PATH: {}".format(vtest_path))
+    logging.info("VROUTER_SOCKET_PATH: {}".format(socket_path))
+    logging.info("VTEST_PY_VENV_PATH: {}".format(vtest_py_venv_path))
+    logging.info("VTEST_ONLY_MODE: {}".format(args['vtest_only']))
+    logging.info("VROUTER_ONLY_MODE: {}".format(args['vrouter_only']))
+    logging.info("TEST PARAM: {}".format(test_opt))
+
+    if args['vtest_only']:
+        os.environ["VTEST_ONLY_MODE"] = "1"
+        if(os.system('pidof contrail-vrouter-dpdk') != 0):
+            print("Error! You have specified vtest_only, but there is")
+            print("no vrouter running. Please check!")
+            return
+    else:
+        os.environ["VTEST_ONLY_MODE"] = "0"
+
+    if args['vrouter_only']:
+        os.environ["VROUTER_ONLY_MODE"] = "1"
+        exec_cmd = 'taskset 0x6 {} --no-daemon --no-huge --vr_packet_sz 2048 \
+                --vr_socket_dir {}'.format(vrouter_path, socket_path)
+        logging.info("Running cmd {}".format(exec_cmd))
+        os.execlp("taskset", "taskset", "0x6", vrouter_path,
+                  "--no-daemon", "--no-huge", "--vr_packet_sz",
+                  "2048", "--vr_socket_dir", socket_path)
+        return
+    else:
+        os.environ["VROUTER_ONLY_MODE"] = "0"
+
+    extension = None
+    if(test_opt is not None and test_opt.find('.py') != -1):
+        extension = test_opt.split('.')[1]
+    if(extension == 'py'):
+        cmd = "cp {} {}/tests".format(test_opt, vtest_py_venv_path)
+        logging.info("Running cmd {}".format(cmd))
+        os.system(cmd)
+
+    logging.info("Entering venv")
+    os.chdir(vtest_py_venv_path)
+    result = 0
+    cmd = None
+    if args['all']:
+        logging.info("Executing all the tests in ./tests dir ..")
+        if(args['xml'] is not None):
+            cmd = 'pytest ./tests --junitxml=result.xml'
+        else:
+            cmd = 'pytest ./tests/'
+    elif args['pycodestyle']:
+        logging.info("Running pycodestyle check ..")
+        cmd = 'pycodestyle ./lib/vtest* ./tests/*'
+    elif args['flake']:
+        logging.info("Running flake check ..")
+        cmd = 'flake8 ./lib/vtest* ./tests/*'
+    else:
+        if(test_opt is not None):
+            logging.info("Executing test file %s ..".format(test_opt))
+            if(args['xml'] is not None):
+                cmd = 'pytest -s ./tests/{} --junitxml=result.xml'.format(
+                    test_opt)
+            else:
+                cmd = 'pytest -s ./tests/{}'.format(test_opt)
+    run_command(cmd)
+    logging.info("Exiting venv")
+
+    if(result != 0):
+        logging.error("Script execution failed")
+        return
+
+
+def run_command(cmd):
+    if(cmd is None):
+        logging.info("No command to run. Exiting!!!")
+        return
+    logging.info("Running cmd - {}".format(cmd))
+    cmd = "source ./bin/activate;" + cmd + "; deactivate"
+    os.system(cmd)
+
+
+def main():
+    cmd = sys.argv
+    parse(cmd)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/pylib/flow.py
@@ -0,0 +1,173 @@
+#
+# Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+#
+
+import vtconst
+import vtest_common
+from vtest_object_base import *
+
+from vr_py_sandesh.vr_py.ttypes import *
+
+
+class Flow(VTestObjectBase, vr_flow_req, VTestCommon):
+    def __init__(
+            self,
+            sip_l,
+            sip_u,
+            dip_l,
+            dip_u,
+            sport,
+            dport,
+            proto,
+            family,
+            idx=-1,
+            ridx=-1,
+            flags=vtconst.VR_FLOW_FLAG_ACTIVE,
+            flow_nh_id=0,
+            src_nh_idx=0,
+            qos_id=-1,
+            action=vtconst.VR_FLOW_ACTION_FORWARD,
+            ecmp_nh_index=-1,
+            flow_vrf=0,
+            rflow_nh_id=0,
+            **kwargs):
+        super(Flow, self).__init__()
+        vr_flow_req.__init__(self)
+        self.fr_op = vtconst.FLOW_OPER_SET
+        self.fr_index = idx
+        self.fr_rindex = ridx
+        self.fr_flow_sip_l = sip_l
+        self.fr_flow_sip_u = sip_u
+        self.fr_flow_dip_l = dip_l
+        self.fr_flow_dip_u = dip_u
+        self.fr_family = family
+        self.fr_flow_proto = proto
+        self.fr_flow_sport = socket.htons(sport)
+        self.fr_flow_dport = socket.htons(dport)
+        self.fr_flags = flags
+        self.fr_flow_nh_id = flow_nh_id
+        self.fr_src_nh_index = src_nh_idx
+        self.fr_ecmp_nh_index = ecmp_nh_index
+        self.fr_action = action
+        self.fr_qos_id = qos_id
+        # set reverse flow params as mirror of forward flow by default
+        self.rflow_sip_u = self.fr_flow_dip_u
+        self.rflow_sip_l = self.fr_flow_dip_l
+        self.rflow_dip_u = self.fr_flow_sip_u
+        self.rflow_dip_l = self.fr_flow_sip_l
+        self.rflow_sport = self.fr_flow_dport
+        self.rflow_dport = self.fr_flow_sport
+        self.rflow_nh_id = rflow_nh_id
+        self.sreq_class = vr_flow_req.__name__
+
+    def sync_and_add_reverse_flow(self):
+        self.sync(resp_required=True)
+        fr_indx = self.get_fr_index()
+        fr_genid = self.get_fr_gen_id()
+        print("Forward flow index {}".format(int(fr_indx)))
+        print("Forward flow gen index {}".format(int(fr_genid)))
+
+        flow = vr_flow_req()
+        flow.fr_op = vtconst.FLOW_OPER_SET
+        flow.fr_index = -1
+        flow.fr_rindex = fr_indx
+        flow.fr_flow_sip_l = self.fr_flow_dip_l
+        flow.fr_flow_sip_u = self.fr_flow_dip_u
+        flow.fr_flow_dip_l = self.fr_flow_sip_l
+        flow.fr_flow_dip_u = self.fr_flow_sip_u
+        flow.fr_flow_sport = self.fr_flow_dport
+        flow.fr_flow_dport = self.fr_flow_sport
+        flow.fr_flow_proto = self.fr_flow_proto
+        flow.fr_action = vtconst.VR_FLOW_ACTION_FORWARD
+        flow.fr_flags = vtconst.VR_FLOW_FLAG_ACTIVE | vtconst.VR_RFLOW_VALID
+        flow.fr_family = self.fr_family
+        flow.fr_flow_nh_id = self.rflow_nh_id
+        flow.fr_src_nh_index = self.rflow_nh_id
+        flow.fr_qos_id = self.fr_qos_id
+        flow.fr_ecmp_nh_index = self.fr_ecmp_nh_index
+        flow.fr_flow_vrf = self.fr_flow_vrf
+        flow.rflow_sip_u = flow.fr_flow_dip_u
+        flow.rflow_sip_l = flow.fr_flow_dip_l
+        flow.rflow_dip_u = flow.fr_flow_sip_u
+        flow.rflow_dip_l = flow.fr_flow_sip_l
+        flow.rflow_nh_id = flow.fr_flow_nh_id
+        flow.rflow_sport = self.rflow_sport
+
+        resp_file = self.send_sandesh_req(flow, self.VT_RESPONSE_REQD)
+        rfr_indx = self.parse_xml_field(resp_file, "fresp_index")
+
+        # Update forward flow
+        self.fr_index = fr_indx
+        self.fr_rindex = int(rfr_indx)
+        self.fr_gen_id = int(fr_genid)
+        self.fr_flags |= vtconst.VR_RFLOW_VALID
+        resp_file = self.send_sandesh_req(self, self.VT_RESPONSE_REQD)
+
+    def link_flow(self, flow2):
+        fr_indx = self.get_fr_index()
+        fr_genid = self.get_fr_gen_id()
+
+        # update flow2
+        flow2.fr_rindex = fr_indx
+        flow2.fr_flags |= vtconst.VR_RFLOW_VALID
+        resp_file = self.send_sandesh_req(flow2, self.VT_RESPONSE_REQD)
+        rfr_indx = self.parse_xml_field(resp_file, "fresp_index")
+
+        # Update forward flow
+        self.fr_index = fr_indx
+        self.fr_rindex = int(rfr_indx)
+        self.fr_gen_id = int(fr_genid)
+        self.fr_flags |= vtconst.VR_RFLOW_VALID
+        resp_file = self.send_sandesh_req(self, self.VT_RESPONSE_REQD)
+
+    def add_multiple():
+        pass
+
+    def get(self, key):
+        return super(Flow, self).get(key)
+
+    def get_fr_index(self):
+        return int(self.get('fresp_index'))
+
+    def get_fr_gen_id(self):
+        return int(self.get('fresp_gen_id'))
+
+
+class InetFlow(Flow):
+    def __init__(self, sip, dip, sport, dport, proto, family=socket.AF_INET,
+                 **kwargs):
+        super(InetFlow, self).__init__(
+            self.vt_ipv4(sip),
+            0,
+            self.vt_ipv4(dip),
+            0,
+            sport,
+            dport,
+            proto,
+            family=vtconst.AF_INET,
+            **kwargs)
+
+
+class Inet6Flow(Flow):
+    def __init__(self, sip6, dip6, proto, sport, dport, family=socket.AF_INET6,
+                 **kwargs):
+        super(Inet6Flow, self).__init__(
+            sip6[0],
+            sip6[1],
+            dip6[0],
+            dip6[1],
+            vtconst.AF_INET6,
+            proto,
+            sport,
+            dport,
+            **kwargs)
+
+
+class NatFlow(Flow):
+    def __init__():
+        pass
+
+
+class MirrorFlow(Flow):
+    def __init__():
+        pass
