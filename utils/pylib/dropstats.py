cbd74927999f243e963eb842b509a3e64dff61ce
Manish Sahu <msahu@juniper.net>
2020-01-12 23:12:50 +0530

    vtest_py infra improvements
    1. Convert run_test.sh to python script
    2. Move tests directory to $SB/vrouter/tests
    3. Move vtest_py vtest specific libraries to $SB/vrouter/utils/pylib
    4. Implement business logic for classes mentioned in skeletal
       implementation CEM-11303
    5. Move UTs' test_basic, test_basic1, test_basic2, test_vrf_table
       to new infra
    
    Closes-Jira-Bug: CEM-8197
    Change-Id: Ia369f2c46e0e040b6c5f7f9670a88608fd915f60

---
+++ utils/pylib/dropstats.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+#
+
+from object_base import *
+from common import *
+from vr_py_sandesh.vr_py.ttypes import *
+
+
+class DropStats(ObjectBase, vr_drop_stats_req):
+    """
+    DropStats class to get dropstats
+    """
+
+    def __init__(self, *args, **kwargs):
+        vr_drop_stats_req.__init__(self)
+        super(DropStats, self).__init__(*args, **kwargs)
+        self.h_op = constants.SANDESH_OPER_GET
+        self.sreq_class = vr_drop_stats_req.__name__
+
+    def get_vds_invalid_arp(self):
+        """Queries vrouter and returns vds_invalid_arp value from response file"""
+        return int(self.get("vds_invalid_arp"))
+
+    def delete(self):
+        pass
