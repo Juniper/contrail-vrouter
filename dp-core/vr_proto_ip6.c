f98c88387cee43eb60c1af138b4abe6bda60aed1
Kiran Kasim <kirankn@juniper.net>
2020-02-15 06:48:06 -0800

    Support for fragmentation in ECMP with packet mode
    1) Handle fragmentation in case of ECMP with packet mode
    2) Pass packets through the fragment assembler in this case
    3) For these packets, enable "frag_calc" where, the last fragment
    cleans up the fragment entry
    4) Handle cases where fragment addition in fragment hash table
    return error
    5) Make the fragment hash table and assembler table scanning
    more aggressive
    6) Move all the #defines of fragmentation in one place
    7) Add more comments to important fragmentation functions
    
    Change-Id: If6f7683fa89f7ce494fbeae7f15d38b1fb91af34
    Closes-Jira-Bug: CEM-11118

--- dp-core/vr_proto_ip6.c
+++ dp-core/vr_proto_ip6.c
@@ -247,7 +247,7 @@ vr_inet6_flow_is_fat_flow(struct vrouter *router, struct vr_packet *pkt,
 static int
 vr_inet6_proto_flow(struct vrouter *router, unsigned short vrf,
         struct vr_packet *pkt, uint16_t vlan, struct vr_ip6 *ip6,
-        struct vr_flow *flow_p, uint8_t valid_fkey_params, bool frag_calc)
+        struct vr_flow *flow_p, uint8_t valid_fkey_params, unsigned short custom)
 {
     int ret = 0;
     uint8_t ip6_nxt;
@@ -273,13 +273,13 @@ vr_inet6_proto_flow(struct vrouter *router, unsigned short vrf,
             if ((unsigned char *)ip6 == pkt_network_header(pkt)) {
                 ret = vr_inet6_form_flow(router, vrf, pkt, vlan,
                         (struct vr_ip6 *)(icmph + 1), flow_p,
-                        valid_fkey_params, frag_calc);
+                        valid_fkey_params, custom);
                 if (ret)
                     return ret;
 
                 vr_inet6_flow_swap(flow_p);
             } else {
-                return -1;
+                return VP_DROP_ICMP_ERROR;
             }
 
             return 0;
@@ -334,7 +334,7 @@ vr_inet6_proto_flow(struct vrouter *router, unsigned short vrf,
 static int
 vr_inet6_fragment_flow(struct vrouter *router, unsigned short vrf,
         struct vr_packet *pkt, uint16_t vlan, struct vr_ip6 *ip6,
-        struct vr_flow *flow_p, uint8_t valid_fkey_params, bool frag_calc)
+        struct vr_flow *flow_p, uint8_t valid_fkey_params, unsigned short custom)
 {
     uint16_t sport, dport;
     unsigned short nh_id;
@@ -342,27 +342,24 @@ vr_inet6_fragment_flow(struct vrouter *router, unsigned short vrf,
     struct vr_ip6_frag *v6_frag;
 
     frag = vr_fragment_get(router, vrf,
-            (struct vr_ip *)(pkt_network_header(pkt)));
+            (struct vr_ip *)(pkt_network_header(pkt)), custom);
     if (!frag) {
-        return -1;
+        return VP_DROP_NO_FRAG_ENTRY;
     }
 
     sport = frag->f_sport;
     dport = frag->f_dport;
     v6_frag = (struct vr_ip6_frag *)(ip6 + 1);
 
-    if (frag_calc) {
-        frag->f_received += (ntohs(ip6->ip6_plen) - sizeof(struct vr_ip6_frag));
-        if (vr_ip6_fragment_tail(ip6)) {
-            frag->f_expected = (((ntohs(v6_frag->ip6_frag_offset)) >>
-                              VR_IP6_FRAG_OFFSET_BITS) * 8) +
-                              ntohs(ip6->ip6_plen) - sizeof(struct vr_ip6_frag);
-        }
-        if (frag->f_received == frag->f_expected) {
-            vr_fragment_del(router->vr_fragment_table, frag);
-        }
+    frag->f_received += (ntohs(ip6->ip6_plen) - sizeof(struct vr_ip6_frag));
+    if (vr_ip6_fragment_tail(ip6)) {
+        frag->f_expected = (((ntohs(v6_frag->ip6_frag_offset)) >>
+                          VR_IP6_FRAG_OFFSET_BITS) * 8) +
+                          ntohs(ip6->ip6_plen) - sizeof(struct vr_ip6_frag);
     }
-
+    if (frag->f_received == frag->f_expected) {
+        vr_fragment_del(router->vr_fragment_table, frag);
+    }
 
     nh_id = vr_inet_flow_nexthop(pkt, vlan);
     vr_inet6_fill_flow(flow_p, nh_id, (unsigned char *)&ip6->ip6_src,
@@ -375,16 +372,16 @@ vr_inet6_fragment_flow(struct vrouter *router, unsigned short vrf,
 int
 vr_inet6_form_flow(struct vrouter *router, unsigned short vrf,
         struct vr_packet *pkt, uint16_t vlan, struct vr_ip6 *ip6,
-        struct vr_flow *flow_p, uint8_t valid_fkey_params, bool frag_calc)
+        struct vr_flow *flow_p, uint8_t valid_fkey_params, unsigned short custom)
 {
     int ret = 0;
 
     if (vr_ip6_transport_header_valid(ip6)) {
         ret = vr_inet6_proto_flow(router, vrf, pkt, vlan, ip6, flow_p,
-                valid_fkey_params, frag_calc);
+                valid_fkey_params, custom);
     } else {
         ret = vr_inet6_fragment_flow(router, vrf, pkt, vlan, ip6,
-                                        flow_p, valid_fkey_params, frag_calc);
+                                        flow_p, valid_fkey_params, custom);
     }
 
     return ret;
@@ -394,20 +391,22 @@ vr_inet6_form_flow(struct vrouter *router, unsigned short vrf,
 int
 vr_inet6_get_flow_key(struct vrouter *router, unsigned short vrf,
         struct vr_packet *pkt, uint16_t vlan, struct vr_flow *flow_p,
-        uint8_t valid_fkey_params)
+        uint8_t valid_fkey_params, unsigned short custom)
 {
     int ret;
     struct vr_ip6 *ip6;
 
     ip6 = (struct vr_ip6 *)pkt_network_header(pkt);
     ret = vr_inet6_form_flow(router, vrf, pkt, vlan, ip6, flow_p,
-            valid_fkey_params, false);
+            valid_fkey_params, custom);
     if (ret < 0)
         return ret;
 
     if (vr_ip6_fragment_head(ip6)) {
-        vr_v6_fragment_add(router, vrf, ip6, flow_p->flow6_sport,
-                flow_p->flow6_dport);
+        ret = vr_v6_fragment_add(router, vrf, ip6, flow_p->flow6_sport,
+                flow_p->flow6_dport, custom);
+        if (ret < 0)
+            return VP_DROP_NO_MEMORY;
     }
 
     return ret;
@@ -458,8 +457,10 @@ vr_inet6_flow_lookup(struct vrouter *router, struct vr_packet *pkt,
         return FLOW_FORWARD;
 
     if (vr_ip6_fragment_head(ip6)) {
-        vr_v6_fragment_add(router, fmd->fmd_dvrf, ip6, flow_p->flow6_sport,
-                flow_p->flow6_dport);
+        ret = vr_v6_fragment_add(router, fmd->fmd_dvrf, ip6, flow_p->flow6_sport,
+                flow_p->flow6_dport, 0);
+        if (ret < 0)
+            return VP_DROP_NO_MEMORY;
         if (vr_enqueue_to_assembler){
             pkt_c = vr_pclone(pkt);
             if (pkt_c) {
