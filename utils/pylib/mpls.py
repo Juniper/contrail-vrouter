2806b388060ca10706acab7073a5a8d5067da37f
YashikaBadaya <ybadaya@juniper.net>
2020-02-27 12:04:22 +0530

    Addition of new test cases as per new infrastructure
    1. Have added vxlan.py
    2. Changed usage of python list to python dictionary in object_base.py
    3. Have added implementation of new packets to packet.py for new test cases
    4. Have added basic test cases for the implementation of mirror and mpls classes
    
    Closes-Jira-Bug: CEM-11749
    Blueprint: https://github.com/Juniper/contrail-specs/blob/master/vtest_py.md
    Change-Id: I61ba8bdc7a27ab385f8e3f7718d57fa00aa53ca7

--- utils/pylib/mpls.py
+++ utils/pylib/mpls.py
@@ -25,7 +25,7 @@ class Mpls(ObjectBase, vr_mpls_req):
         Mpls nexthop id
     """
 
-    def __init__(self, mr_label, mr_rid=None, mr_nhid=None):
+    def __init__(self, mr_label, mr_rid=0, mr_nhid=None):
         super(Mpls, self).__init__()
         vr_mpls_req.__init__(self)
         self.h_op = constants.SANDESH_OPER_ADD
@@ -35,8 +35,24 @@ class Mpls(ObjectBase, vr_mpls_req):
             self.mr_nhid = mr_nhid
         self.sreq_class = vr_mpls_req.__name__
 
+    def label(self):
+        """Returns mr_label"""
+        return self.mr_label
+
     def get(self, key):
         """
         Queries vrouter and return the key value from the response xml file
         """
+        self.h_op = constants.SANDESH_OPER_GET
         return super(Mpls, self).get(key)
+
+    def get_mr_label(self):
+        """
+        Queries vrouter and returns the mr_label value from the response
+        xml file
+        """
+        return int(self.get('mr_label'))
+
+    def delete(self):
+        self.h_op = constants.SANDESH_OPER_DEL
+        super(Mpls, self).delete()
