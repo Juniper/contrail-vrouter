63c349eac3cf6ee5917b3ff17fb1fc8b6b74403f
ybadaya <ybadaya@juniper.net>
2020-03-13 10:54:40 +0530

    Port XML test cases to new infrastructure
    1. Have added vxlan.py
    2. Changed usage of python list to python dictionary in object_base.py
    3. Have added implementation of new packets to packet.py for new test cases
    4. Have added basic test cases for the implementation of mirror and mpls classes
    
    Closes-Jira-Bug: CEM-11749
    Blueprint: https://github.com/Juniper/contrail-specs/blob/master/vtest_py.md
    Change-Id: I61ba8bdc7a27ab385f8e3f7718d57fa00aa53ca7

--- tests/test_fabric_to_vm_intra_vn.py
+++ tests/test_fabric_to_vm_intra_vn.py
@@ -6,6 +6,124 @@ sys.path.append(os.getcwd())
 sys.path.append(os.getcwd() + '/lib/')
 from imports import *  # noqa
 
+'''
+vif --list
+-----------
+Vrouter Interface Table
+
+Flags: P=Policy, X=Cross Connect, S=Service Chain, Mr=Receive Mirror
+       Mt=Transmit Mirror, Tc=Transmit Checksum Offload, L3=Layer 3,
+       L2=Layer 2, D=DHCP, Vp=Vhost Physical, Pr=Promiscuous,
+       Vnt=Native Vlan Tagged, Mnp=No MAC Proxy, Dpdk=DPDK PMD Interface,
+       Rfl=Receive Filtering Offload, Mon=Interface is Monitored
+       Uuf=Unknown Unicast Flood, Vof=VLAN insert/strip offload,
+       Df=Drop New Flows, L=MAC Learning Enabled,
+       Proxy=MAC Requests Proxied Always, Er=Etree Root,
+       Mn=Mirror without Vlan Tag, Ig=Igmp Trap Enabled
+
+vif0/0      PCI: Mock
+            Type:Physical HWaddr:00:1b:21:bb:f9:48 IPaddr:0.0.0.0
+            Vrf:0 Mcast Vrf:65535 Flags:L3L2Vp QOS:0 Ref:7
+            RX port   packets:1 errors:0 syscalls:1
+            RX queue errors to lcore 0 0 0 0 0 0 0 0 0 0 0
+            RX packets:1  bytes:88 errors:0
+            TX packets:0  bytes:0 errors:0
+            Drops:0
+
+vif0/1      PMD: vhost0 Mock
+            Type:Host HWaddr:00:1b:21:bb:f9:48 IPaddr:8.0.0.2
+            Vrf:0 Mcast Vrf:65535 Flags:L3D QOS:0 Ref:6
+            RX queue errors to lcore 0 0 0 0 0 0 0 0 0 0 0
+            RX packets:0  bytes:0 errors:0
+            TX packets:0  bytes:0 errors:0
+            Drops:0
+
+vif0/2      Socket: unix Mock
+            Type:Agent HWaddr:00:00:5e:00:01:00 IPaddr:0.0.0.0
+            Vrf:65535 Mcast Vrf:65535 Flags:L3 QOS:0 Ref:5
+            RX queue errors to lcore 0 0 0 0 0 0 0 0 0 0 0
+            RX packets:0  bytes:0 errors:0
+            TX packets:0  bytes:0 errors:0
+            Drops:0
+
+vif0/5      PMD: tapc2234cd0-55
+            Type:Virtual HWaddr:00:00:5e:00:01:00 IPaddr:1.1.1.3
+            Vrf:5 Mcast Vrf:5 Flags:PL3L2D QOS:0 Ref:7
+            RX queue errors to lcore 0 0 0 0 0 0 0 0 0 0 0
+            RX packets:0  bytes:0 errors:0
+            TX packets:1  bytes:42 errors:0
+            Drops:0
+            TX port   packets:1 errors:0 syscalls:1
+
+nh --list
+---------
+[root@090c8246aecd vtest_py]# $utils/nh --sock-dir $sock --list
+Id:0          Type:Drop           Fmly: AF_INET  Rid:0  Ref_cnt:1021  Vrf:0
+              Flags:Valid,
+
+Id:10         Type:Receive        Fmly: AF_INET  Rid:0  Ref_cnt:2     Vrf:1
+              Flags:Valid, Policy(R), Etree Root,
+              Oif:1
+
+Id:21         Type:Tunnel         Fmly: AF_INET  Rid:0  Ref_cnt:1     Vrf:0
+              Flags:Valid, MPLSoUDP, Etree Root,
+              Oif:0 Len:14 Data:00 1b 21 bb f9 46 00 1b 21 bb f9 48 08 00
+              Sip:8.0.0.2 Dip:8.0.0.3
+
+Id:38         Type:Encap          Fmly: AF_INET  Rid:0  Ref_cnt:1     Vrf:5
+              Flags:Valid, Policy, Etree Root,
+              EncapFmly:0000 Oif:5 Len:14
+              Encap Data: 02 c2 23 4c d0 55 00 00 5e 00 01 00 08 00
+
+Id:44         Type:Encap          Fmly:AF_BRIDGE  Rid:0  Ref_cnt:2    Vrf:5
+              Flags:Valid, Policy, Etree Root,
+              EncapFmly:0000 Oif:5 Len:14
+              Encap Data: 02 c2 23 4c d0 55 00 00 5e 00 01 00 08 00
+
+flow -l
+-------
+[root@090c8246aecd vtest_py]# $utils/flow --sock-dir $sock -l
+Flow table(size 80609280, entries 629760)
+
+Entries: Created 0 Added 2 Deleted 0 Changed 1Processed 0 Used Overflow
+entries 0
+(Created Flows/CPU: 0 0 0 0 0 0 0 0 0 0 0)(oflows 0)
+
+Action:F=Forward, D=Drop N=NAT(S=SNAT, D=DNAT, Ps=SPAT, Pd=DPAT,
+L=Link Local Port)
+Other:K(nh)=Key_Nexthop, S(nh)=RPF_Nexthop
+Flags:E=Evicted, Ec=Evict Candidate, N=New Flow, M=Modified Dm=Delete Marked
+TCP(r=reverse):S=SYN, F=FIN, R=RST, C=HalfClose, E=Established, D=Dead
+
+    Index                Source:Port/Destination:Port               Proto(V)
+-----------------------------------------------------------------------------
+   255616<=>410748       1.1.1.3:4145                                1 (5)
+                         1.1.1.5:0
+(Gen: 1, K(nh):38, Action:F, Flags:, QOS:-1, S(nh):38,  Stats:0/0,
+SPort 60847, TTL 0, Sinfo 0.0.0.0)
+
+   410748<=>255616       1.1.1.5:4145                                 1 (5)
+                         1.1.1.3:0
+(Gen: 1, K(nh):38, Action:F, Flags:, QOS:-1, S(nh):21,  Stats:1/42,
+SPort 50789, TTL 0, Sinfo 8.0.0.3)
+
+rt --dump 0
+-----------
+[root@090c8246aecd vtest_py]#
+$utils/rt --sock-dir $sock --dump 0 --family inet | grep "8.0.0.2 "\\">"
+8.0.0.2/32             32            T          -             10        -
+
+mpls --dump
+-----------
+[root@090c8246aecd vtest_py]# $utils/mpls --sock-dir $sock --dump
+MPLS Input Label Map
+
+   Label    NextHop
+-------------------
+      42        44
+
+'''
+
 # anything with *test* will be assumed by pytest as a test
 
 
@@ -46,47 +164,49 @@ class TestFabricToVmIntraVn(unittest.TestCase):
             vrf=5,
             mcast_vrf=5,
             nh_idx=38)
-
         # Add vif Nexthop
         self.vif_nh = EncapNextHop(
             encap_oif_id=self.tenant_vif.idx(),
             encap="02 c2 23 4c d0 55 00 00 5e 00 01 00 08 00",
             nh_idx=38,
-            nh_vrf=5)
-
+            nh_vrf=5,
+            nh_flags=constants.NH_FLAG_POLICY_ENABLED |
+            constants.NH_FLAG_ETREE_ROOT)
         # Add underlay Receive NH
         self.underlay_rnh = ReceiveNextHop(
             encap_oif_id=self.vhost0_vif.idx(),
             nh_idx=10,
-            nh_vrf=1)
-
+            nh_vrf=1,
+            nh_flags=constants.NH_FLAG_RELAXED_POLICY |
+            constants.NH_FLAG_ETREE_ROOT)
         # Add underlay Route
         self.underlay_route = InetRoute(
             vrf=0,
             prefix="8.0.0.2",
-            nh_idx=10)
-
+            nh_idx=10,
+            rtr_label_flags=constants.VR_RT_ARP_TRAP_FLAG)
         # Add Encap L2 Nexthop for overlay
         self.l2_nh = EncapNextHop(
             encap_oif_id=self.tenant_vif.idx(),
             encap="02 c2 23 4c d0 55 00 00 5e 00 01 00 08 00",
             nh_idx=44,
             nh_family=constants.AF_BRIDGE,
-            nh_vrf=5)
-
+            nh_vrf=5,
+            nh_flags=constants.NH_FLAG_POLICY_ENABLED |
+            constants.NH_FLAG_ETREE_ROOT)
         # Add MPLS entry for overlay
         self.mpls_entry = Mpls(
             mr_label=42,
             mr_nhid=44)
-
         # Add tunnel NH (for src validation)
         self.tunnel_nh = TunnelNextHopV4(
             encap_oif_id=self.fabric_interface.idx(),
             encap="00 1b 21 bb f9 46 00 1b 21 bb f9 48 08 00",
             tun_sip="8.0.0.2",
             tun_dip="8.0.0.3",
-            nh_idx=21)
-
+            nh_idx=21,
+            nh_flags=constants.NH_FLAG_TUNNEL_UDP_MPLS |
+            constants.NH_FLAG_ETREE_ROOT)
         ObjectBase.sync_all()
 
         # Add forward and reverse flow
@@ -122,10 +242,11 @@ class TestFabricToVmIntraVn(unittest.TestCase):
 
         # send mplsudp packet from fabric
         icmp_inner = IcmpPacket(
-            sip='2.2.2.3',
+            sip='1.1.1.5',
             dip='1.1.1.3',
             smac='02:c2:23:4c:d0:55',
             dmac='02:e7:03:ea:67:f1',
+            id=4145,
             icmp_type=constants.ECHO_REPLY)
         pkt = icmp_inner.get_packet()
         self.assertIsNotNone(pkt)
@@ -138,6 +259,7 @@ class TestFabricToVmIntraVn(unittest.TestCase):
             dmac='00:1b:21:bb:f9:48',
             sport=53363,
             dport=6635,
+            id=10,
             inner_pkt=pkt)
         pkt = mpls.get_packet()
         pkt.show()
@@ -172,31 +294,12 @@ class TestFabricToVmIntraVn(unittest.TestCase):
             flags=constants.VIF_FLAG_HBS_RIGHT)
         hbs_r_vif.sync()
 
-        self.vif_nh.set_nh_flags(
-            nh_flags=constants.NH_FLAG_POLICY_ENABLED)
-        self.vif_nh.sync()
-
-        self.underlay_rnh.set_nh_flags(
-            nh_flags=constants.NH_FLAG_RELAXED_POLICY)
-        self.underlay_rnh.sync()
-
-        self.underlay_route.rtr_label_flags = constants.VR_RT_ARP_TRAP_FLAG
-        self.underlay_route.sync()
-
-        self.l2_nh.set_nh_flags(
-            nh_flags=constants.NH_FLAG_POLICY_ENABLED)
-        self.l2_nh.sync()
-
         # Add Bridge Route
         bridge_route = BridgeRoute(
             vrf=5,
             mac_str="02:c2:23:4c:d0:55",
             nh_idx=44)
         bridge_route.sync()
-
-        self.tunnel_nh.set_nh_flags(
-            nh_flags=constants.NH_FLAG_TUNNEL_UDP_MPLS)
-        self.tunnel_nh.sync()
 
         # Add hbs-l and hbs-r in the vrf table
         vrf = Vrf(
